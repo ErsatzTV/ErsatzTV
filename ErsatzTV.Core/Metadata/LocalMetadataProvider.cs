a3e20826a5ceb7446be1e0949822a47affc7b3ec
Jason Dove <jason@jasondove.me>
2021-02-11 13:33:59 -0600

    Movie metadata fixes (#9)
    * reorganize metadata parsing; only attempt to parse appropriate media type based on media source configuration
    
    * add fallback metadata for movie sources
    
    * only request read access for nfo metadata
    
    * fix tests

--- ErsatzTV.Core/Metadata/LocalMetadataProvider.cs
+++ ErsatzTV.Core/Metadata/LocalMetadataProvider.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using ErsatzTV.Core.Domain;
 using ErsatzTV.Core.Interfaces.Metadata;
 using ErsatzTV.Core.Interfaces.Repositories;
 using LanguageExt;
+using Microsoft.Extensions.Logging;
 using static LanguageExt.Prelude;
 
 namespace ErsatzTV.Core.Metadata
 {
     public class LocalMetadataProvider : ILocalMetadataProvider
     {
+        private static readonly XmlSerializer MovieSerializer = new(typeof(MovieNfo));
+        private static readonly XmlSerializer TvShowSerializer = new(typeof(TvShowEpisodeNfo));
+        private readonly ILogger<LocalMetadataProvider> _logger;
+
         private readonly IMediaItemRepository _mediaItemRepository;
 
-        public LocalMetadataProvider(IMediaItemRepository mediaItemRepository) =>
+        public LocalMetadataProvider(IMediaItemRepository mediaItemRepository, ILogger<LocalMetadataProvider> logger)
+        {
             _mediaItemRepository = mediaItemRepository;
+            _logger = logger;
+        }
 
         public async Task RefreshMetadata(MediaItem mediaItem)
         {
             Option<MediaMetadata> maybeMetadata = await LoadMetadata(mediaItem);
             MediaMetadata metadata =
-                maybeMetadata.IfNone(() => FallbackMetadataProvider.GetFallbackMetadata(mediaItem.Path));
+                maybeMetadata.IfNone(() => FallbackMetadataProvider.GetFallbackMetadata(mediaItem));
             await ApplyMetadataUpdate(mediaItem, metadata);
         }
 
@@ -50,54 +57,72 @@ namespace ErsatzTV.Core.Metadata
             string nfoFileName = Path.ChangeExtension(mediaItem.Path, "nfo");
             if (nfoFileName == null || !File.Exists(nfoFileName))
             {
+                _logger.LogDebug("NFO file does not exist at {Path}", nfoFileName);
                 return None;
             }
 
-            var tvShowSerializer = new XmlSerializer(typeof(TvShowEpisodeNfo));
-            var movieSerializer = new XmlSerializer(typeof(MovieNfo));
+            if (!(mediaItem.Source is LocalMediaSource localMediaSource))
+            {
+                _logger.LogDebug("Media source {Name} is not a local media source", mediaItem.Source.Name);
+                return None;
+            }
 
-            TryAsync<object> tvShowAttempt = TryAsync(
-                async () =>
-                {
-                    await using FileStream fileStream = File.Open(nfoFileName, FileMode.Open);
-                    return tvShowSerializer.Deserialize(fileStream);
-                });
-            TryAsync<object> movieAttempt = TryAsync(
-                async () =>
-                {
-                    await using FileStream fileStream = File.Open(nfoFileName, FileMode.Open);
-                    return movieSerializer.Deserialize(fileStream);
-                });
-            return await choice(tvShowAttempt, movieAttempt).Match<object, Option<MediaMetadata>>(
-                result =>
-                {
-                    switch (result)
+            return localMediaSource.MediaType switch
+            {
+                MediaType.Movie => await LoadMovieMetadata(nfoFileName),
+                MediaType.TvShow => await LoadTvShowMetadata(nfoFileName),
+                _ => None
+            };
+        }
+
+        private async Task<Option<MediaMetadata>> LoadTvShowMetadata(string nfoFileName)
+        {
+            try
+            {
+                await using FileStream fileStream = File.Open(nfoFileName, FileMode.Open, FileAccess.Read);
+                Option<TvShowEpisodeNfo> maybeNfo = TvShowSerializer.Deserialize(fileStream) as TvShowEpisodeNfo;
+                return maybeNfo.Match<Option<MediaMetadata>>(
+                    nfo => new MediaMetadata
                     {
-                        case TvShowEpisodeNfo nfo:
-                            return new MediaMetadata
-                            {
-                                MediaType = MediaType.TvShow,
-                                Title = nfo.ShowTitle,
-                                Subtitle = nfo.Title,
-                                Description = nfo.Outline,
-                                EpisodeNumber = nfo.Episode,
-                                SeasonNumber = nfo.Season,
-                                Aired = GetAired(nfo.Aired)
-                            };
-                        case MovieNfo nfo:
-                            return new MediaMetadata
-                            {
-                                MediaType = MediaType.Movie,
-                                Title = nfo.Title,
-                                Description = nfo.Outline,
-                                ContentRating = nfo.ContentRating,
-                                Aired = GetAired(nfo.Premiered)
-                            };
-                        default:
-                            return None;
-                    }
-                },
-                None);
+                        MediaType = MediaType.TvShow,
+                        Title = nfo.ShowTitle,
+                        Subtitle = nfo.Title,
+                        Description = nfo.Outline,
+                        EpisodeNumber = nfo.Episode,
+                        SeasonNumber = nfo.Season,
+                        Aired = GetAired(nfo.Aired)
+                    },
+                    None);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogDebug(ex, "Failed to read TV nfo metadata from {Path}", nfoFileName);
+                return None;
+            }
+        }
+
+        private async Task<Option<MediaMetadata>> LoadMovieMetadata(string nfoFileName)
+        {
+            try
+            {
+                await using FileStream fileStream = File.Open(nfoFileName, FileMode.Open, FileAccess.Read);
+                Option<MovieNfo> maybeNfo = MovieSerializer.Deserialize(fileStream) as MovieNfo;
+                return maybeNfo.Match<Option<MediaMetadata>>(
+                    nfo => new MediaMetadata
+                    {
+                        MediaType = MediaType.Movie,
+                        Title = nfo.Title,
+                        Description = nfo.Outline,
+                        ContentRating = nfo.ContentRating,
+                        Aired = GetAired(nfo.Premiered)
+                    },
+                    None);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogDebug(ex, "Failed to read Movie nfo metadata from {Path}", nfoFileName);
+                return None;
+            }
         }
 
         private static DateTime? GetAired(string aired)
