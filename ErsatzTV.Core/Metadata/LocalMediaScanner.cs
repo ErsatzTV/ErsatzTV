ebff29d6cd5b1d51062b77552441e6e0ddbd7984
Jason Dove <jason@jasondove.me>
2021-02-11 09:35:32 -0600

    Xml and scanner fixes (#7)
    * flush xml, use utf8
    
    * scan ts files
    
    * use links instead of icons for m3u, xmltv, api

--- ErsatzTV.Core/Metadata/LocalMediaScanner.cs
+++ ErsatzTV.Core/Metadata/LocalMediaScanner.cs
@@ -60,7 +60,7 @@ namespace ErsatzTV.Core.Metadata
             var knownExtensions = new List<string>
             {
                 ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".m4p", ".m4v",
-                ".avi", ".wmv", ".mov", ".mkv"
+                ".avi", ".wmv", ".mov", ".mkv", ".ts"
             };
 
             var allFiles = Directory.GetFiles(localMediaSource.Folder, "*", SearchOption.AllDirectories)
