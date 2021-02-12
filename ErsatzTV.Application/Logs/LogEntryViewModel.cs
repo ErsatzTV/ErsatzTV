de6578379ef0a73ca8447a7f42442e97ac0abb4b
Jason Dove <jason@jasondove.me>
2021-02-12 16:18:44 -0600

    add basic logging ui (#14)

---
+++ ErsatzTV.Application/Logs/LogEntryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErsatzTV.Application.Logs
+{
+    public record LogEntryViewModel(
+        int Id,
+        DateTime Timestamp,
+        string Level,
+        string Exception,
+        string RenderedMessage,
+        string Properties);
+}
---
+++ ErsatzTV.Infrastructure/Data/LogContext.cs
@@ -0,0 +1,21 @@
+﻿using ErsatzTV.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErsatzTV.Infrastructure.Data
+{
+    public class LogContext : DbContext
+    {
+        public LogContext(DbContextOptions<LogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LogEntry> LogEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(LogContext).Assembly);
+        }
+    }
+}
