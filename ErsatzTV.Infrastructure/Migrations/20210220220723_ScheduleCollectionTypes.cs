3adeb04062ecac1a596b784982d1cd7ec83e5502
Jason Dove <jason@jasondove.me>
2021-02-21 15:59:30 -0600

    move cleanup to new migration

--- ErsatzTV.Infrastructure/Migrations/20210220220723_ScheduleCollectionTypes.cs
+++ ErsatzTV.Infrastructure/Migrations/20210220220723_ScheduleCollectionTypes.cs
@@ -112,17 +112,6 @@ namespace ErsatzTV.Infrastructure.Migrations
                 "TelevisionShows",
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Restrict);
-
-            // delete program schedule items that referenced television collections (that no longer exist)
-            migrationBuilder.Sql(
-                "delete from ProgramScheduleItems where MediaCollectionId not in (select Id from SimpleMediaCollections)");
-            
-            // delete television collections that no longer exist/work
-            migrationBuilder.Sql(
-                "delete from MediaCollections where Id not in (select Id from SimpleMediaCollections)");
-            
-            // delete all posters so they are all re-cached with a higher resolution
-            migrationBuilder.Sql("update MediaItems set Poster = null");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
