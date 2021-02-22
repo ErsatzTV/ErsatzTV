38ab6c00abdc2f58589ce7aa1bb223956d721f5b
Jason Dove <jason@jasondove.me>
2021-02-22 13:01:58 -0600

    media source scan interval (#28)
    * scan media sources once every six hours
    
    * cleanup
    
    * force scan from ui

---
+++ ErsatzTV.Core/Errors/MediaSourceRecentlyScanned.cs
@@ -0,0 +1,10 @@
+﻿namespace ErsatzTV.Core.Errors
+{
+    public class MediaSourceRecentlyScanned : BaseError
+    {
+        public MediaSourceRecentlyScanned(string folder) :
+            base($"Media source {folder} was already scanned recently; skipping scan.")
+        {
+        }
+    }
+}
