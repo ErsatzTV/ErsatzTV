653515258293007ce145b239ef1e50894f61d660
Jason Dove <jason@jasondove.me>
2021-02-27 18:45:03 -0600

    fix deleting show library paths

---
+++ ErsatzTV.Infrastructure/Data/Configurations/Metadata/SeasonMetadataConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ErsatzTV.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ErsatzTV.Infrastructure.Data.Configurations
+{
+    public class SeasonMetadataConfiguration : IEntityTypeConfiguration<SeasonMetadata>
+    {
+        public void Configure(EntityTypeBuilder<SeasonMetadata> builder)
+        {
+            builder.ToTable("SeasonMetadata");
+
+            builder.HasMany(em => em.Artwork)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
