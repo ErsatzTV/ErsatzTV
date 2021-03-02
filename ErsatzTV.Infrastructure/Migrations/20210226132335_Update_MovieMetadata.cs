e780cde4874d3195bbeb173b65536d7f39a9fd95
Jason Dove <jason@jasondove.me>
2021-03-02 04:57:08 -0600

    support null movie metadata last write time

--- ErsatzTV.Infrastructure/Migrations/20210226132335_Update_MovieMetadata.cs
+++ ErsatzTV.Infrastructure/Migrations/20210226132335_Update_MovieMetadata.cs
@@ -11,7 +11,7 @@ namespace ErsatzTV.Infrastructure.Migrations
 
             migrationBuilder.Sql(
                 @$"INSERT INTO NewMovieMetadata (Outline, Plot, Tagline, MovieId, MetadataKind, Title, OriginalTitle, SortTitle, ReleaseDate, DateAdded, DateUpdated)
-SELECT mm.Outline, mm.Plot, mm.Tagline, m.Id, mm.Source, mm.Title, null, mm.SortTitle, mm.Premiered, '{now}', mm.LastWriteTime
+SELECT mm.Outline, mm.Plot, mm.Tagline, m.Id, mm.Source, mm.Title, null, mm.SortTitle, mm.Premiered, '{now}', IFNULL(mm.LastWriteTime, '0001-01-01 00:00:00')
 FROM MovieMetadata mm
 INNER JOIN Movie m ON mm.MovieId = m.Id");
         }
