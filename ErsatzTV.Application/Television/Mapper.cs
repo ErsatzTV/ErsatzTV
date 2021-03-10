a3f46b2a8d6c7d94dd31f941e8cf797134598117
Jason Dove <jason@jasondove.me>
2021-03-10 05:28:42 -0600

    update tv show ui

--- ErsatzTV.Application/Television/Mapper.cs
+++ ErsatzTV.Application/Television/Mapper.cs
@@ -12,7 +12,8 @@ namespace ErsatzTV.Application.Television
                 show.ShowMetadata.HeadOrNone().Map(m => m.Title ?? string.Empty).IfNone(string.Empty),
                 show.ShowMetadata.HeadOrNone().Map(m => m.Year?.ToString() ?? string.Empty).IfNone(string.Empty),
                 show.ShowMetadata.HeadOrNone().Map(m => m.Plot ?? string.Empty).IfNone(string.Empty),
-                show.ShowMetadata.HeadOrNone().Map(GetPoster).IfNone(string.Empty));
+                show.ShowMetadata.HeadOrNone().Map(GetPoster).IfNone(string.Empty),
+                show.ShowMetadata.HeadOrNone().Map(GetFanArt).IfNone(string.Empty));
 
         internal static TelevisionSeasonViewModel ProjectToViewModel(Season season) =>
             new(
@@ -32,12 +33,14 @@ namespace ErsatzTV.Application.Television
                 episode.EpisodeMetadata.HeadOrNone().Map(m => m.Plot ?? string.Empty).IfNone(string.Empty),
                 episode.EpisodeMetadata.HeadOrNone().Map(GetThumbnail).IfNone(string.Empty));
 
-        private static string GetPoster(Metadata metadata) =>
-            Optional(metadata.Artwork.FirstOrDefault(a => a.ArtworkKind == ArtworkKind.Poster))
-                .Match(a => a.Path, string.Empty);
+        private static string GetPoster(Metadata metadata) => GetArtwork(metadata, ArtworkKind.Poster);
 
-        private static string GetThumbnail(Metadata metadata) =>
-            Optional(metadata.Artwork.FirstOrDefault(a => a.ArtworkKind == ArtworkKind.Thumbnail))
+        private static string GetFanArt(Metadata metadata) => GetArtwork(metadata, ArtworkKind.FanArt);
+
+        private static string GetThumbnail(Metadata metadata) => GetArtwork(metadata, ArtworkKind.Thumbnail);
+
+        private static string GetArtwork(Metadata metadata, ArtworkKind artworkKind) =>
+            Optional(metadata.Artwork.FirstOrDefault(a => a.ArtworkKind == artworkKind))
                 .Match(a => a.Path, string.Empty);
     }
 }
