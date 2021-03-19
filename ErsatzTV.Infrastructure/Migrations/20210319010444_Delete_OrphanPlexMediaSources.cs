9ae18cc93b9a854c5fa2653d02d1bde04e42af3e
Jason Dove <jason@jasondove.me>
2021-03-18 20:07:31 -0500

    delete orphan plex media sources

---
+++ ErsatzTV.Infrastructure/Migrations/20210319010444_Delete_OrphanPlexMediaSources.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Delete_OrphanPlexMediaSources : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.Sql(
+                @"DELETE FROM MediaSource WHERE Id NOT IN
+                (SELECT Id FROM LocalMediaSource UNION ALL SELECT Id FROM PlexMediaSource)");
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
