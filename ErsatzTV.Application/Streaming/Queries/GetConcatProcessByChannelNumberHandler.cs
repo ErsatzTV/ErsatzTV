90f775aab4beb267d254809cfec49fc07b10fc45
Jason Dove <jason@jasondove.me>
2021-03-30 20:08:57 -0500

    ffmpeg tweaks (#121)
    * save reports from ffmpeg concat process
    
    * let ffmpeg determine thread count by default
    
    * disable stdin for ffmpeg processes

--- ErsatzTV.Application/Streaming/Queries/GetConcatProcessByChannelNumberHandler.cs
+++ ErsatzTV.Application/Streaming/Queries/GetConcatProcessByChannelNumberHandler.cs
@@ -5,30 +5,38 @@ using ErsatzTV.Core.Domain;
 using ErsatzTV.Core.FFmpeg;
 using ErsatzTV.Core.Interfaces.Repositories;
 using LanguageExt;
-using static LanguageExt.Prelude;
 
 namespace ErsatzTV.Application.Streaming.Queries
 {
     public class GetConcatProcessByChannelNumberHandler : FFmpegProcessHandler<GetConcatProcessByChannelNumber>
     {
+        private readonly IConfigElementRepository _configElementRepository;
         private readonly FFmpegProcessService _ffmpegProcessService;
 
         public GetConcatProcessByChannelNumberHandler(
             IChannelRepository channelRepository,
             IConfigElementRepository configElementRepository,
             FFmpegProcessService ffmpegProcessService)
-            : base(channelRepository, configElementRepository) =>
+            : base(channelRepository, configElementRepository)
+        {
+            _configElementRepository = configElementRepository;
             _ffmpegProcessService = ffmpegProcessService;
+        }
 
-        protected override Task<Either<BaseError, Process>> GetProcess(
+        protected override async Task<Either<BaseError, Process>> GetProcess(
             GetConcatProcessByChannelNumber request,
             Channel channel,
-            string ffmpegPath) =>
-            Right<BaseError, Process>(
-                _ffmpegProcessService.ConcatChannel(
-                    ffmpegPath,
-                    channel,
-                    request.Scheme,
-                    request.Host)).AsTask();
+            string ffmpegPath)
+        {
+            bool saveReports = await _configElementRepository.GetValue<bool>(ConfigElementKey.FFmpegSaveReports)
+                .Map(result => result.IfNone(false));
+
+            return _ffmpegProcessService.ConcatChannel(
+                ffmpegPath,
+                saveReports,
+                channel,
+                request.Scheme,
+                request.Host);
+        }
     }
 }
