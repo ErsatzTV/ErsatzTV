86b5723d6fa38937d7abe822bcf092cab23cd123
Jason Dove <jason@jasondove.me>
2021-04-03 06:36:20 -0500

    refactor local metadata provider

---
+++ ErsatzTV.Core/Metadata/Nfo/TvShowEpisodeNfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace ErsatzTV.Core.Metadata.Nfo
+{
+    [XmlRoot("episodedetails")]
+    public class TvShowEpisodeNfo
+    {
+        [XmlElement("showtitle")]
+        public string ShowTitle { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("episode")]
+        public int Episode { get; set; }
+
+        [XmlElement("season")]
+        public int Season { get; set; }
+
+        [XmlElement("mpaa")]
+        public string ContentRating { get; set; }
+
+        [XmlElement("aired")]
+        public string Aired { get; set; }
+
+        [XmlElement("plot")]
+        public string Plot { get; set; }
+    }
+}
