db25310451083df533ff3fdaae5b87885bd8dafb
Jason Dove <jason@jasondove.me>
2021-04-04 20:56:38 -0500

    clean up fragment letter anchor code

---
+++ ErsatzTV/Shared/FragmentLetterAnchor.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ErsatzTV.Application.MediaCards;
+using Microsoft.AspNetCore.Components;
+
+namespace ErsatzTV.Shared
+{
+    public partial class FragmentLetterAnchor<TCard> where TCard : MediaCardViewModel
+    {
+        [Parameter]
+        public RenderFragment<TCard> ChildContent { get; set; }
+
+        [Parameter]
+        public List<TCard> Cards { get; set; }
+    }
+}
