3f406ac556afc00d854fc35a38578f935ad01f68
Jason Dove <jason@jasondove.me>
2021-04-06 07:25:05 -0500

    log viewer improvements (#147)

--- ErsatzTV.Application/Logs/Mapper.cs
+++ ErsatzTV.Application/Logs/Mapper.cs
@@ -1,16 +1,45 @@
-﻿using ErsatzTV.Core.Domain;
+﻿using System;
+using System.Collections.Generic;
+using ErsatzTV.Core.Domain;
+using Newtonsoft.Json.Linq;
+using Serilog.Events;
 
 namespace ErsatzTV.Application.Logs
 {
     internal static class Mapper
     {
-        internal static LogEntryViewModel ProjectToViewModel(LogEntry logEntry) =>
-            new(
+        internal static LogEntryViewModel ProjectToViewModel(LogEntry logEntry)
+        {
+            string message = logEntry.RenderedMessage;
+            if (!string.IsNullOrWhiteSpace(logEntry.Properties))
+            {
+                foreach (KeyValuePair<string, JToken> property in JObject.Parse(logEntry.Properties))
+                {
+                    var token = $"{{{property.Key}}}";
+                    if (message.Contains(token))
+                    {
+                        message = message.Replace(token, property.Value.ToString());
+                    }
+
+                    var destructureToken = $"{{@{property.Key}}}";
+                    if (message.Contains(destructureToken))
+                    {
+                        message = message.Replace(destructureToken, property.Value.ToString());
+                    }
+                }
+            }
+
+            if (!Enum.TryParse(logEntry.Level, out LogEventLevel level))
+            {
+                level = LogEventLevel.Debug;
+            }
+
+            return new LogEntryViewModel(
                 logEntry.Id,
                 logEntry.Timestamp,
-                logEntry.Level,
+                level,
                 logEntry.Exception,
-                logEntry.RenderedMessage,
-                logEntry.Properties);
+                message);
+        }
     }
 }
