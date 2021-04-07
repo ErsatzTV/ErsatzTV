cb6023cf35287f998af906a8ddba12d8aa37ccb6
Jason Dove <jason@jasondove.me>
2021-04-07 18:42:01 -0500

    not null artist id

--- ErsatzTV.Core/Domain/Metadata/ArtistMetadata.cs
+++ ErsatzTV.Core/Domain/Metadata/ArtistMetadata.cs
@@ -7,7 +7,7 @@ namespace ErsatzTV.Core.Domain
         public string Disambiguation { get; set; }
         public string Biography { get; set; }
         public string Formed { get; set; }
-        public int? ArtistId { get; set; }
+        public int ArtistId { get; set; }
         public Artist Artist { get; set; }
         public List<Style> Styles { get; set; }
         public List<Mood> Moods { get; set; }
--- ErsatzTV.Core/Domain/Metadata/MusicVideoMetadata.cs
+++ ErsatzTV.Core/Domain/Metadata/MusicVideoMetadata.cs
@@ -1,9 +1,13 @@
-﻿namespace ErsatzTV.Core.Domain
+﻿using System;
+
+namespace ErsatzTV.Core.Domain
 {
     public class MusicVideoMetadata : Metadata
     {
         public string Album { get; set; }
         public string Plot { get; set; }
+        // TODO: delete this
+        [Obsolete("Need to delete this")]
         public string Artist { get; set; }
         public int MusicVideoId { get; set; }
         public MusicVideo MusicVideo { get; set; }
