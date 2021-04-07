37c742c23ff8bd55f54798be27b72d095fb3ac3e
Jason Dove <jason@jasondove.me>
2021-04-07 06:36:02 -0500

    clean up genre, tag, studio orphans

---
+++ ErsatzTV.Infrastructure/Migrations/20210407113048_Delete_Orphan_GenreTagStudio.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Delete_Orphan_GenreTagStudio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"DELETE FROM Genre
+                WHERE MovieMetadataId NOT IN (SELECT Id FROM MovieMetadata)
+                OR ShowMetadataId NOT IN (SELECT Id FROM Show)
+                OR SeasonMetadataId NOT IN (SELECT Id FROM Season)
+                OR EpisodeMetadataId NOT IN (SELECT Id FROM Episode)
+                OR MusicVideoMetadataId NOT IN (SELECT Id FROM MusicVideoMetadata)");
+
+            migrationBuilder.Sql(
+                @"DELETE FROM Tag
+                WHERE MovieMetadataId NOT IN (SELECT Id FROM MovieMetadata)
+                OR ShowMetadataId NOT IN (SELECT Id FROM Show)
+                OR SeasonMetadataId NOT IN (SELECT Id FROM Season)
+                OR EpisodeMetadataId NOT IN (SELECT Id FROM Episode)
+                OR MusicVideoMetadataId NOT IN (SELECT Id FROM MusicVideoMetadata)");
+
+            migrationBuilder.Sql(
+                @"DELETE FROM Studio
+                WHERE MovieMetadataId NOT IN (SELECT Id FROM MovieMetadata)
+                OR ShowMetadataId NOT IN (SELECT Id FROM Show)
+                OR SeasonMetadataId NOT IN (SELECT Id FROM Season)
+                OR EpisodeMetadataId NOT IN (SELECT Id FROM Episode)
+                OR MusicVideoMetadataId NOT IN (SELECT Id FROM MusicVideoMetadata)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
