bfa290790be365d1bc807b8cf0e01d728f960475
Jason Dove <jason@jasondove.me>
2021-04-07 16:36:20 -0500

    trim parsed music video titles (#152)

---
+++ ErsatzTV.Infrastructure/Migrations/20210407212003_Update_MusicVideoMetadata_Title.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Update_MusicVideoMetadata_Title : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"UPDATE MusicVideoMetadata SET DateUpdated = '0001-01-01 00:00:00' WHERE MetadataKind = 0");
+
+            migrationBuilder.Sql(
+                @"UPDATE LibraryPath SET LastScan = '0001-01-01 00:00:00' WHERE LibraryId IN
+                (SELECT Id FROM Library WHERE MediaKind = 3)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
