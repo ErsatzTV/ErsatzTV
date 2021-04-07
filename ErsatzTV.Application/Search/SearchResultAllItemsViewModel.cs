436c9119faf53ea1fff52edd6f54118807c27fc5
Jason Dove <jason@jasondove.me>
2021-04-06 20:43:44 -0500

    add all search results to collection (#150)

---
+++ ErsatzTV.Application/Search/SearchResultAllItemsViewModel.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+namespace ErsatzTV.Application.Search
+{
+    public record SearchResultAllItemsViewModel(List<int> MovieIds, List<int> ShowIds, List<int> MusicVideoIds);
+}
