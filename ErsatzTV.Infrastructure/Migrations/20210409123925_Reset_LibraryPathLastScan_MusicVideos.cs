05bf7fa2a6c5d3902d2cbedb842af435f766cbc6
Jason Dove <jason@jasondove.me>
2021-04-09 07:42:13 -0500

    force music video library scan (#159)

---
+++ ErsatzTV.Infrastructure/Migrations/20210409123925_Reset_LibraryPathLastScan_MusicVideos.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Reset_LibraryPathLastScan_MusicVideos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.Sql(
+                @"UPDATE LibraryPath SET LastScan = '0001-01-01 00:00:00' WHERE Id IN
+                (SELECT LP.Id FROM LibraryPath LP INNER JOIN Library L on L.Id = LP.LibraryId WHERE MediaKind = 3)");
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
