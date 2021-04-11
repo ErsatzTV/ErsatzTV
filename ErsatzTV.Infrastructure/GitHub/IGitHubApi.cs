0d3a9ba6412b38e1043b8e1a9c40d19e626b6e4c
Jason Dove <jason@jasondove.me>
2021-04-11 12:34:41 -0500

    show release notes on home page (#165)

---
+++ ErsatzTV.Infrastructure/GitHub/IGitHubApi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ErsatzTV.Infrastructure.GitHub.Models;
+using Refit;
+
+namespace ErsatzTV.Infrastructure.GitHub
+{
+    [Headers("Accept: application/vnd.github.v3+json", "User-Agent: jasongdove/ErsatzTV")]
+    public interface IGitHubApi
+    {
+        [Get("/repos/jasongdove/ErsatzTV/releases")]
+        public Task<List<GitHubTag>> GetReleases();
+        
+        [Get("/repos/jasongdove/ErsatzTV/releases/tags/{tag}")]
+        public Task<GitHubTag> GetTag(string tag);
+    }
+}
