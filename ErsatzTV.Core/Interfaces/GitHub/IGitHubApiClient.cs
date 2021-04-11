04b48bee41e3caeca57f71668ecf3665857000b2
Jason Dove <jason@jasondove.me>
2021-04-11 12:34:27 -0500

    show release notes on home page

---
+++ ErsatzTV.Core/Interfaces/GitHub/IGitHubApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using LanguageExt;
+
+namespace ErsatzTV.Core.Interfaces.GitHub
+{
+    public interface IGitHubApiClient
+    {
+        Task<Either<BaseError, string>> GetLatestReleaseNotes();
+        Task<Either<BaseError, string>> GetReleaseNotes(string tag);
+    }
+}
---
+++ ErsatzTV.Infrastructure/GitHub/GitHubApiClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using ErsatzTV.Core;
+using ErsatzTV.Core.Interfaces.GitHub;
+using LanguageExt;
+using Refit;
+
+namespace ErsatzTV.Infrastructure.GitHub
+{
+    public class GitHubApiClient : IGitHubApiClient
+    {
+        public async Task<Either<BaseError, string>> GetLatestReleaseNotes()
+        {
+            try
+            {
+                IGitHubApi service = RestService.For<IGitHubApi>("https://api.github.com");
+                return await service.GetReleases().Map(releases => releases.Head().Body);
+            }
+            catch (Exception ex)
+            {
+                return BaseError.New(ex.ToString());
+            }
+        }
+
+        public async Task<Either<BaseError, string>> GetReleaseNotes(string tag)
+        {
+            try
+            {
+                IGitHubApi service = RestService.For<IGitHubApi>("https://api.github.com");
+                return await service.GetTag(tag).Map(t => t.Body);
+            }
+            catch (Exception ex)
+            {
+                return BaseError.New(ex.ToString());
+            }
+        }
+    }
+}
