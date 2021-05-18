ff54e52bb43b0b10e17015f346560045cf9e3ad5
Jason Dove <jason@jasondove.me>
2021-05-18 11:00:21 -0500

    another multi-episode bugfix

--- ErsatzTV.Core/Scheduling/GroupedMediaItem.cs
+++ ErsatzTV.Core/Scheduling/GroupedMediaItem.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using ErsatzTV.Core.Domain;
 
 namespace ErsatzTV.Core.Scheduling
 {
+    [DebuggerDisplay("{First}")]
     public record GroupedMediaItem(MediaItem First, List<MediaItem> Additional);
 }
