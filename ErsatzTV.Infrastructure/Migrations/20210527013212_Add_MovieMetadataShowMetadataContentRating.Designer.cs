c2e90784a16150ffcaf077e646200fe1783d7385
Jason Dove <jason@jasondove.me>
2021-05-26 20:36:39 -0500

    add new columns

--- ErsatzTV.Infrastructure/Migrations/20210527013212_Add_MovieMetadataShowMetadataContentRating.Designer.cs
+++ ErsatzTV.Infrastructure/Migrations/20210527013212_Add_MovieMetadataShowMetadataContentRating.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ErsatzTV.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ErsatzTV.Infrastructure.Migrations
 {
     [DbContext(typeof(TvContext))]
-    partial class TvContextModelSnapshot : ModelSnapshot
+    [Migration("20210527013212_Add_MovieMetadataShowMetadataContentRating")]
+    partial class Add_MovieMetadataShowMetadataContentRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ErsatzTV.Infrastructure/Migrations/20210527013212_Add_MovieMetadataShowMetadataContentRating.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Add_MovieMetadataShowMetadataContentRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentRating",
+                table: "ShowMetadata",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentRating",
+                table: "MovieMetadata",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentRating",
+                table: "ShowMetadata");
+
+            migrationBuilder.DropColumn(
+                name: "ContentRating",
+                table: "MovieMetadata");
+        }
+    }
+}
