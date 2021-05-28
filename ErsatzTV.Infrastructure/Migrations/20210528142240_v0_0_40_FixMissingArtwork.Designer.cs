54adb2bb68c8bb6b5ebf2d5f6f7f8943fa350544
Jason Dove <jason@jasondove.me>
2021-05-28 09:34:47 -0500

    fix missing artwork (#225)

--- ErsatzTV.Infrastructure/Migrations/20210528142240_v0_0_40_FixMissingArtwork.Designer.cs
+++ ErsatzTV.Infrastructure/Migrations/20210528142240_v0_0_40_FixMissingArtwork.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ErsatzTV.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ErsatzTV.Infrastructure.Migrations
 {
     [DbContext(typeof(TvContext))]
-    partial class TvContextModelSnapshot : ModelSnapshot
+    [Migration("20210528142240_v0_0_40_FixMissingArtwork")]
+    partial class v0_0_40_FixMissingArtwork
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
