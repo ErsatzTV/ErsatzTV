da5148affd9bf047b9c5066dfc80b1b8bd5388e3
Jason Dove <jason@jasondove.me>
2021-06-07 20:34:24 -0500

    quickly skip missing files during plex library scan (#251)

--- ErsatzTV.Core/Interfaces/Plex/IPlexPathReplacementService.cs
+++ ErsatzTV.Core/Interfaces/Plex/IPlexPathReplacementService.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ErsatzTV.Core.Domain;
 
 namespace ErsatzTV.Core.Interfaces.Plex
 {
     public interface IPlexPathReplacementService
     {
         Task<string> GetReplacementPlexPath(int libraryPathId, string path);
+        string GetReplacementPlexPath(List<PlexPathReplacement> pathReplacements, string path, bool log = true);
     }
 }
