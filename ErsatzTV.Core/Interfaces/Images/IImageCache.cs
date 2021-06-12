0750a0712f5f649f0a27aa103377bad6fa6927ec
Jason Dove <jason@jasondove.me>
2021-06-12 06:16:52 -0500

    allow animated channel watermarks (#255)

--- ErsatzTV.Core/Interfaces/Images/IImageCache.cs
+++ ErsatzTV.Core/Interfaces/Images/IImageCache.cs
@@ -10,5 +10,6 @@ namespace ErsatzTV.Core.Interfaces.Images
         Task<Either<BaseError, string>> SaveArtworkToCache(byte[] imageBuffer, ArtworkKind artworkKind);
         Task<Either<BaseError, string>> CopyArtworkToCache(string path, ArtworkKind artworkKind);
         string GetPathForImage(string fileName, ArtworkKind artworkKind, Option<int> maybeMaxHeight);
+        Task<bool> IsAnimated(string fileName);
     }
 }
