963650178708178932b845dc3bab9962de977799
Jason Dove <jason@jasondove.me>
2021-06-14 13:11:35 -0500

    save schedules and playouts table page sizes

--- ErsatzTV.Core/Domain/ConfigElementKey.cs
+++ ErsatzTV.Core/Domain/ConfigElementKey.cs
@@ -17,6 +17,8 @@
         public static ConfigElementKey HDHRTunerCount => new("hdhr.tuner_count");
         public static ConfigElementKey ChannelsPageSize => new("pages.channels.page_size");
         public static ConfigElementKey CollectionsPageSize => new("pages.collections.page_size");
+        public static ConfigElementKey SchedulesPageSize => new("pages.schedules.page_size");
+        public static ConfigElementKey PlayoutsPageSize => new("pages.playouts.page_size");
         public static ConfigElementKey LibraryRefreshInterval => new("scanner.library_refresh_interval");
     }
 }
