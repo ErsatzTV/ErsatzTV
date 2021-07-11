a197d9a415c0104b5a1e3ceb70948dc29e18bd78
Jason Dove <jason@jasondove.me>
2021-07-11 10:46:29 -0500

    fix loading playout anchors

---
+++ ErsatzTV.Infrastructure/Migrations/20210711154337_Fix_PlayoutAnchorInFlood.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    public partial class Fix_PlayoutAnchorInFlood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Playout SET Anchor_InFlood = 0 WHERE Anchor_InFlood IS NULL");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Anchor_InFlood",
+                table: "Playout",
+                type: "INTEGER",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
