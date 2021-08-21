9d076277813b0689871f71cd4772cd2cca6bb347
Jason Dove <jason@jasondove.me>
2021-08-21 05:57:39 -0500

    fix ffprobe parsing in some cultures (#337)

---
+++ ErsatzTV.Core.Tests/Metadata/LocalStatisticsProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using ErsatzTV.Core.Domain;
+using ErsatzTV.Core.Interfaces.Metadata;
+using ErsatzTV.Core.Interfaces.Repositories;
+using ErsatzTV.Core.Metadata;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace ErsatzTV.Core.Tests.Metadata
+{
+    [TestFixture]
+    public class LocalStatisticsProviderTests
+    {
+        [Test]
+        // this needs to be a culture where '.' is a group separator
+        [SetCulture("it-IT")] 
+        public void Test()
+        {
+            var provider = new LocalStatisticsProvider(
+                new Mock<IMetadataRepository>().Object,
+                new Mock<ILocalFileSystem>().Object,
+                new Mock<ILogger<LocalStatisticsProvider>>().Object);
+
+            var input = new LocalStatisticsProvider.FFprobe(
+                new LocalStatisticsProvider.FFprobeFormat("123.45"),
+                new List<LocalStatisticsProvider.FFprobeStream>());
+
+            MediaVersion result = provider.ProjectToMediaVersion("test", input);
+
+            result.Duration.Should().Be(TimeSpan.FromSeconds(123.45));
+        }
+    }
+}
