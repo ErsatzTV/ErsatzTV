d7936023a76131675818fdc8925e4c5b47d6e8b0
Jason Dove <jason@jasondove.me>
2021-09-10 10:07:34 -0500

    overwrite smart collections

---
+++ ErsatzTV.Application/MediaCollections/Queries/GetAllSmartCollections.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace ErsatzTV.Application.MediaCollections.Queries
+{
+    public record GetAllSmartCollections : IRequest<List<SmartCollectionViewModel>>;
+}
