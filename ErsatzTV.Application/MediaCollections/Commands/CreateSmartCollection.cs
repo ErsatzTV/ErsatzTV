1c9e643732754a0dbdc71a4c68c927a6cd4b8459
Jason Dove <jason@jasondove.me>
2021-09-10 09:33:34 -0500

    start to add smart collections

---
+++ ErsatzTV.Application/MediaCollections/Commands/CreateSmartCollection.cs
@@ -0,0 +1,9 @@
+﻿using ErsatzTV.Core;
+using LanguageExt;
+using MediatR;
+
+namespace ErsatzTV.Application.MediaCollections.Commands
+{
+    public record CreateSmartCollection
+        (string Query, string Name) : IRequest<Either<BaseError, SmartCollectionViewModel>>;
+}
