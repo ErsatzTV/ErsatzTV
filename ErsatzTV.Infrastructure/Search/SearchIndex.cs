ad2685fb2ec30b0849993164e0602063c558458f
Jason Dove <jason@jasondove.me>
2021-09-10 21:11:14 -0500

    add released_inthelast queries (#357)

--- ErsatzTV.Infrastructure/Search/SearchIndex.cs
+++ ErsatzTV.Infrastructure/Search/SearchIndex.cs
@@ -148,8 +148,8 @@ namespace ErsatzTV.Infrastructure.Search
             };
             using var analyzerWrapper = new PerFieldAnalyzerWrapper(analyzer, customAnalyzers);
             QueryParser parser = !string.IsNullOrWhiteSpace(searchField)
-                ? new QueryParser(AppLuceneVersion, searchField, analyzerWrapper)
-                : new MultiFieldQueryParser(AppLuceneVersion, new[] { TitleField }, analyzerWrapper);
+                ? new RelativeDateQueryParser(AppLuceneVersion, searchField, analyzerWrapper)
+                : new RelativeDateMultiFieldQueryParser(AppLuceneVersion, new[] { TitleField }, analyzerWrapper);
             parser.AllowLeadingWildcard = true;
             Query query = ParseQuery(searchQuery, parser);
             var filter = new DuplicateFilter(TitleAndYearField);
