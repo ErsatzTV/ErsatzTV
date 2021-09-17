ad157c40d066d56ce44c52d05cd017c1aa7c9d02
Jason Dove <jason@jasondove.me>
2021-09-17 17:59:59 -0500

    add some health checks to home page (#374)

---
+++ ErsatzTV.Core/Health/IHealthCheckService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ErsatzTV.Core.Health
+{
+    public interface IHealthCheckService
+    {
+        Task<List<HealthCheckResult>> PerformHealthChecks();
+    }
+}
