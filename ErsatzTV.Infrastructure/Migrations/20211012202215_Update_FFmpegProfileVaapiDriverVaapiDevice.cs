9a427052eff963e48ba12b02b47590b1694506d8
Jason Dove <jason@jasondove.me>
2021-10-12 18:22:09 -0500

    fix vaapi migration

--- ErsatzTV.Infrastructure/Migrations/20211012202215_Update_FFmpegProfileVaapiDriverVaapiDevice.cs
+++ ErsatzTV.Infrastructure/Migrations/20211012202215_Update_FFmpegProfileVaapiDriverVaapiDevice.cs
@@ -8,7 +8,7 @@ namespace ErsatzTV.Infrastructure.Migrations
         {
             migrationBuilder.Sql("UPDATE FFmpegProfile SET VaapiDevice = '/dev/dri/renderD128'");
             migrationBuilder.Sql(
-                "UPDATE FFmpegProfile SET VaapiDriver = (SELECT IFNULL(Value, 0) FROM ConfigElement WHERE Key = 'ffmpeg.vaapi_driver')");
+                "UPDATE FFmpegProfile SET VaapiDriver = (SELECT IFNULL((SELECT Value AS A FROM ConfigElement WHERE Key = 'ffmpeg.vaapi_driver'), 0))");
             migrationBuilder.Sql("DELETE FROM ConfigElement WHERE Key = 'ffmpeg.vaapi_driver'");
         }
 
