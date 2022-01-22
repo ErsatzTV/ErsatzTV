e7c9a51e96360ff2e1195e347c970af8e4fae3d9
Jason Dove <jason@jasondove.me>
2022-01-22 14:03:42 -0600

    macos app bundle (#585)
    * test signed app bundle
    
    * fix vars
    
    * fix condition
    
    * typo
    
    * fix quoting
    
    * use recursive signing script
    
    * fix release cleanup
    
    * restore proper ci action

---
+++ scripts/macOS/sign.sh
@@ -0,0 +1,15 @@
+﻿#! /bin/bash
+APP_NAME="release/ErsatzTV.app"
+ENTITLEMENTS="scripts/macOS/ErsatzTV.entitlements"
+SIGNING_IDENTITY="C3BBCFB2D6851FF0DCA6CAC06A3EF1ECE71F9FFF"
+
+find "$APP_NAME/Contents/MacOS/"|while read fname; do
+    if [[ -f $fname ]]; then
+        echo "[INFO] Signing $fname"
+        codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$fname"
+    fi
+done
+
+echo "[INFO] Signing app file"
+
+codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$APP_NAME"
