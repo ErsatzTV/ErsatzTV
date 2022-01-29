33b67f011da26b4b36f02be3c5368f4f8b9c81a6
Jason Dove <jason@jasondove.me>
2022-01-28 21:41:26 -0600

    first pass at native macos app

--- scripts/macOS/bundle.sh
+++ scripts/macOS/bundle.sh
@@ -5,22 +5,14 @@ REPO_ROOT="$SCRIPT_FOLDER/../.."
 
 APP_NAME="$REPO_ROOT/ErsatzTV.app"
 PUBLISH_OUTPUT_DIRECTORY="$REPO_ROOT/publish/."
-INFO_PLIST="$SCRIPT_FOLDER/Info.plist"
-ICON_SOURCE="$REPO_ROOT/artwork/ErsatzTV.icns"
-ICON_FILE="ErsatzTV.icns"
 
 if [ -d "$APP_NAME" ]
 then
     rm -rf "$APP_NAME"
 fi
 
-mkdir "$APP_NAME"
+cd "$REPO_ROOT/ErsatzTV-macOS" || exit
+xcodebuild build
+cp -R "$REPO_ROOT/ErsatzTV-macOS/build/Release/ErsatzTV-macOS.app" "$APP_NAME"
 
-mkdir "$APP_NAME/Contents"
-mkdir "$APP_NAME/Contents/MacOS"
-mkdir "$APP_NAME/Contents/Resources"
-
-cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
-cp "$ICON_SOURCE" "$APP_NAME/Contents/Resources/$ICON_FILE"
 cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
-cp "$SCRIPT_FOLDER/launcher.sh" "$APP_NAME/Contents/MacOS/"
--- scripts/macOS/launcher.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Set the working directory
-DIR=$(cd "$(dirname "$0")"; pwd)
-
-# Run the application
-echo "running from $DIR"
-
-open -a Terminal $DIR/ErsatzTV
