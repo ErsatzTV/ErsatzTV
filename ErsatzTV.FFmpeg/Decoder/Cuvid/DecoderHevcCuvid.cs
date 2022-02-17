a2e1dc8bfbebad5864ccc6027bf6d4eb7392f4e4
Jason Dove <jason@jasondove.me>
2022-02-17 13:28:03 -0600

    don't deinterlace using nvidia decoders (#643)

--- ErsatzTV.FFmpeg/Decoder/Cuvid/DecoderHevcCuvid.cs
+++ ErsatzTV.FFmpeg/Decoder/Cuvid/DecoderHevcCuvid.cs
@@ -2,13 +2,6 @@
 
 public class DecoderHevcCuvid : DecoderBase
 {
-    private readonly FrameState _desiredState;
-
-    public DecoderHevcCuvid(FrameState desiredState)
-    {
-        _desiredState = desiredState;
-    }
-
     public override string Name => "hevc_cuvid";
     public override IList<string> InputOptions
     {
@@ -16,12 +9,6 @@ public class DecoderHevcCuvid : DecoderBase
         {
             IList<string> result =  base.InputOptions;
             
-            if (_desiredState.Deinterlaced)
-            {
-                result.Add("-deint");
-                result.Add("2");
-            }
-
             result.Add("-hwaccel_output_format");
             result.Add("cuda");
 
@@ -30,10 +17,4 @@ public class DecoderHevcCuvid : DecoderBase
     }
 
     protected override FrameDataLocation OutputFrameDataLocation => FrameDataLocation.Hardware;
-    
-    public override FrameState NextState(FrameState currentState)
-    {
-        FrameState result = base.NextState(currentState);
-        return _desiredState.Deinterlaced ? result with { Deinterlaced = true } : result;
-    }
 }
