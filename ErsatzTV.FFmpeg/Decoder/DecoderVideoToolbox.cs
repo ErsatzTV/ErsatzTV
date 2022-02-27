b89608ed8b485917f8eb57231949ba7c16a9e061
Jason Dove <jason@jasondove.me>
2022-02-26 18:43:43 -0600

    fix videotoolbox acceleration with new transcoder

---
+++ ErsatzTV.FFmpeg/Decoder/DecoderVideoToolbox.cs
@@ -0,0 +1,19 @@
+﻿using ErsatzTV.FFmpeg.Format;
+
+namespace ErsatzTV.FFmpeg.Decoder;
+
+public class DecoderVideoToolbox : DecoderBase
+{
+    protected override FrameDataLocation OutputFrameDataLocation => FrameDataLocation.Software;
+    public override string Name => "implicit_videotoolbox";
+    public override IList<string> InputOptions(InputFile inputFile) => Array.Empty<string>();
+
+    // public override FrameState NextState(FrameState currentState)
+    // {
+    //     FrameState nextState = base.NextState(currentState);
+    //
+    //     return currentState.PixelFormat.Match(
+    //         pixelFormat => nextState with { PixelFormat = new PixelFormatNv12(pixelFormat.Name) },
+    //         () => nextState);
+    // }
+}
