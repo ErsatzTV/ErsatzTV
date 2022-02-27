78d37ecba7e74593d97ce192fa87dd235ca6af96
Jason Dove <jason@jasondove.me>
2022-02-26 18:44:44 -0600

    cleanup

--- ErsatzTV.FFmpeg/Decoder/DecoderVideoToolbox.cs
+++ ErsatzTV.FFmpeg/Decoder/DecoderVideoToolbox.cs
@@ -1,19 +1,8 @@
-﻿using ErsatzTV.FFmpeg.Format;
-
-namespace ErsatzTV.FFmpeg.Decoder;
+﻿namespace ErsatzTV.FFmpeg.Decoder;
 
 public class DecoderVideoToolbox : DecoderBase
 {
     protected override FrameDataLocation OutputFrameDataLocation => FrameDataLocation.Software;
     public override string Name => "implicit_videotoolbox";
     public override IList<string> InputOptions(InputFile inputFile) => Array.Empty<string>();
-
-    // public override FrameState NextState(FrameState currentState)
-    // {
-    //     FrameState nextState = base.NextState(currentState);
-    //
-    //     return currentState.PixelFormat.Match(
-    //         pixelFormat => nextState with { PixelFormat = new PixelFormatNv12(pixelFormat.Name) },
-    //         () => nextState);
-    // }
 }
