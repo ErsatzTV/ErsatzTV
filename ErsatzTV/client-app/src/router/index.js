32eedbe53f1cd3591e031ce7b41c583bf7783a39
NotExpectedYet <online@notexpectedyet.com>
2022-03-08 08:45:59 +0000

    add vue-router and initial home page

---
+++ ErsatzTV/client-app/src/router/index.js
@@ -0,0 +1,22 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+import HomePage from "../views/HomePage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        icon: "mdi-view-dashboard-variant",
+        component: HomePage,
+    },
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
+
+export default router;
