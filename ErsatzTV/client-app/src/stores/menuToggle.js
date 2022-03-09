3361bd595191a0054edd1ad25f60811b943fdda4
NotExpectedYet <online@notexpectedyet.com>
2022-03-09 06:25:02 +0000

    use store for menu toggle

---
+++ ErsatzTV/client-app/src/stores/menuToggle.js
@@ -0,0 +1,17 @@
+﻿import { defineStore } from 'pinia'
+
+export const useMenuToggleStore = defineStore('menuToggle', {
+    state: () => {
+        return { miniMenu: false }
+    },
+    getters: {
+        mini(state) {
+            return state.miniMenu
+        },
+    },
+    actions: {
+        toggle() {
+            this.miniMenu = !this.miniMenu
+        },
+    },
+})
