ac277311ab61c32fbd6b91cbe37539922cab9cd7
NotExpectedYet <online@notexpectedyet.com>
2022-03-09 06:25:14 +0000

    replicate old menu

--- ErsatzTV/client-app/src/router/index.js
+++ ErsatzTV/client-app/src/router/index.js
@@ -15,13 +15,202 @@ const routes = [
         }
     },
     {
-        path: "/Settings",
-        name: "Settings",
-        component: HomePage,
+        path: "/channels",
+        name: "Channels",
         meta: {
-            icon: "mdi-home",
+            icon: "mdi-cog",
             disabled: true
         }
+    },
+    {
+        path: "/ffmpeg-profiles",
+        name: "FFmpeg Profiles",
+        meta: {
+            icon: "mdi-cog",
+            disabled: true
+        }
+    },
+    {
+        path: "/watermarks",
+        name: "Watermarks",
+        meta: {
+            icon: "mdi-cog",
+            disabled: true
+        }
+    },
+    {
+        path: "/sources",
+        name: "Media Sources",
+        meta: {
+            icon: "mdi-cog",
+            disabled: false,
+        },
+        children: [
+            {
+                path: "/sources/local",
+                name: "Local",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/sources/emby",
+                name: "Emby",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/sources/jellyfin",
+                name: "Jellyfin",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/sources/plex",
+                name: "Plex",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+        ]
+    },
+    {
+        path: "/media",
+        name: "Media",
+        meta: {
+            icon: "mdi-cog",
+            disabled: false,
+        },
+        children: [
+            {
+                path: "/media/libraries",
+                name: "Libraries",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/trash",
+                name: "Trash",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/tv-shows",
+                name: "TV Shows",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/movies",
+                name: "Movies",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/music-videos",
+                name: "Music Videos",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/other-videos",
+                name: "Other Videos",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/media/songs",
+                name: "Songs",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+        ]
+    },
+    {
+        path: "/lists",
+        name: "Lists",
+        meta: {
+            icon: "mdi-cog",
+            disabled: false,
+        },
+        children: [
+            {
+                path: "/lists/collections",
+                name: "Collections",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/lists/trakt-lists",
+                name: "Trakt Lists",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+            {
+                path: "/lists/filler-presets",
+                name: "Filler Presets",
+                meta: {
+                    icon: "mdi-cog",
+                    disabled: true
+                }
+            },
+        ]
+    },
+    {
+        path: "/schedules",
+        name: "Schedules",
+        meta: {
+            icon: "mdi-cog",
+            disabled: true
+        }
+    },
+    {
+        path: "/playouts",
+        name: "Playouts",
+        meta: {
+            icon: "mdi-cog",
+            disabled: true
+        }
+    },
+    {
+        path: "/settings",
+        name: "Settings",
+        meta: {
+            icon: "mdi-cog",
+            disabled: true
+        }
+    },
+    {
+        path: "/Logs",
+        name: "Logs",
+        meta: {
+            icon: "mdi-card-text",
+            disabled: true
+        },
     },
 ];
 
