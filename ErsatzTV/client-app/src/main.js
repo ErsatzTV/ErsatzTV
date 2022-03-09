ac87470f8a9ba677a003c53cd8e319ca66caa59d
NotExpectedYet <online@notexpectedyet.com>
2022-03-09 04:34:29 +0000

    install store

--- ErsatzTV/client-app/src/main.js
+++ ErsatzTV/client-app/src/main.js
@@ -2,16 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import autoPageTitleMixin from './mixins/autoPageTitle'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-import autoPageTitleMixin from './mixins/autoPageTitle'
 
 Vue.config.productionTip = false
 
-// Mixin to automate the page title when navigating... Will default to "ErsatzTV2 if no title value exported from page.
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+
+// Mixin to automate the page title when navigating... Will default to "ErsatzTV if no title value exported from page.
 Vue.mixin(autoPageTitleMixin)
 
 new Vue({
-  vuetify, router,
+  vuetify, router, pinia,
   render: h => h(App)
 }).$mount('#app')
