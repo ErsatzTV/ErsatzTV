a9a89d04ea14c10a86e82ade29d5e7fbadb7a288
Jason Dove <jason@jasondove.me>
2022-04-27 12:23:37 -0500

    optimize search-index rebuilding (#765)
    * update dependencies
    
    * optimize search-index rebuilding
    
    * cleanup logging

--- ErsatzTV.Core/Interfaces/Repositories/ISearchRepository.cs
+++ ErsatzTV.Core/Interfaces/Repositories/ISearchRepository.cs
@@ -4,10 +4,10 @@ namespace ErsatzTV.Core.Interfaces.Repositories;
 
 public interface ISearchRepository
 {
-    Task<List<int>> GetItemIdsToIndex();
     Task<Option<MediaItem>> GetItemToIndex(int id);
     Task<List<string>> GetLanguagesForShow(Show show);
     Task<List<string>> GetLanguagesForSeason(Season season);
     Task<List<string>> GetLanguagesForArtist(Artist artist);
     Task<List<string>> GetAllLanguageCodes(List<string> mediaCodes);
+    IAsyncEnumerable<MediaItem> GetAllMediaItems();
 }
