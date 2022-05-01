50ca7ff47e7cdf8af00529910128413f0773288c
Jason Dove <jason@jasondove.me>
2022-05-01 13:21:21 -0500

    custom artist nfo reader

--- ErsatzTV.Core/Metadata/Nfo/ArtistNfoReader.cs
+++ ErsatzTV.Core/Metadata/Nfo/ArtistNfoReader.cs
@@ -1,14 +1,12 @@
-﻿using System.Xml.Serialization;
+﻿using System.Xml;
 using Bugsnag;
 using ErsatzTV.Core.Errors;
 using ErsatzTV.Core.Interfaces.Metadata.Nfo;
 
 namespace ErsatzTV.Core.Metadata.Nfo;
 
-public class ArtistNfoReader : IArtistNfoReader
+public class ArtistNfoReader : NfoReader<ArtistNfo>, IArtistNfoReader
 {
-    private static readonly XmlSerializer ArtistSerializer = new(typeof(ArtistNfo));
-
     private readonly IClient _client;
 
     public ArtistNfoReader(IClient client) => _client = client;
@@ -17,8 +15,64 @@ public class ArtistNfoReader : IArtistNfoReader
     {
         try
         {
-            Option<ArtistNfo> nfo = Optional(ArtistSerializer.Deserialize(input) as ArtistNfo);
-            return nfo.ToEither((BaseError)new FailedToReadNfo());
+            var settings = new XmlReaderSettings { Async = true, ConformanceLevel = ConformanceLevel.Fragment };
+            using var reader = XmlReader.Create(input, settings);
+            ArtistNfo nfo = null;
+            var done = false;
+
+            while (!done && await reader.ReadAsync())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        switch (reader.Name.ToLowerInvariant())
+                        {
+                            case "artist":
+                                nfo = new ArtistNfo
+                                {
+                                    Genres = new List<string>(),
+                                    Styles = new List<string>(),
+                                    Moods = new List<string>()
+                                };
+                                break;
+                            case "name":
+                                await ReadStringContent(reader, nfo, (artist, name) => artist.Name = name);
+                                break;
+                            case "disambiguation":
+                                await ReadStringContent(
+                                    reader,
+                                    nfo,
+                                    (artist, disambiguation) => artist.Disambiguation = disambiguation);
+                                break;
+                            case "genre":
+                                await ReadStringContent(reader, nfo, (artist, genre) => artist.Genres.Add(genre));
+                                break;
+                            case "style":
+                                await ReadStringContent(reader, nfo, (artist, style) => artist.Styles.Add(style));
+                                break;
+                            case "mood":
+                                await ReadStringContent(reader, nfo, (artist, mood) => artist.Moods.Add(mood));
+                                break;
+                            case "biography":
+                                await ReadStringContent(
+                                    reader,
+                                    nfo,
+                                    (artist, biography) => artist.Biography = biography);
+                                break;
+                        }
+
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (reader.Name == "artist")
+                        {
+                            done = true;
+                        }
+
+                        break;
+                }
+            }
+
+            return Optional(nfo).ToEither((BaseError)new FailedToReadNfo());
         }
         catch (Exception ex)
         {
