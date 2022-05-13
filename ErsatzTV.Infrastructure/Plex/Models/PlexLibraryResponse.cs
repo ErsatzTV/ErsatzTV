9a8aca573bd88155447c72b9c60c2ee4ff0ebb7a
Jason Dove <jason@jasondove.me>
2022-05-13 12:31:10 -0500

    properly ignore plex other videos libraries

--- ErsatzTV.Infrastructure/Plex/Models/PlexLibraryResponse.cs
+++ ErsatzTV.Infrastructure/Plex/Models/PlexLibraryResponse.cs
@@ -5,6 +5,7 @@ public class PlexLibraryResponse
     public string Key { get; set; }
     public string Title { get; set; }
     public string Type { get; set; }
+    public string Agent { get; set; }
     public int Hidden { get; set; }
     public string Uuid { get; set; }
 }
