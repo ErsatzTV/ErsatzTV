93eba4811753b3effb8d98913e7045da84e636be
Jason Dove <jason@jasondove.me>
2022-07-24 12:23:00 -0500

    fix nvidia capabilities for second-gen maxwell

--- ErsatzTV.FFmpeg/Capabilities/HardwareCapabilitiesFactory.cs
+++ ErsatzTV.FFmpeg/Capabilities/HardwareCapabilitiesFactory.cs
@@ -9,7 +9,8 @@ namespace ErsatzTV.FFmpeg.Capabilities;
 
 public class HardwareCapabilitiesFactory : IHardwareCapabilitiesFactory
 {
-    private const string CacheKey = "ffmpeg.hardware.nvidia.architecture";
+    private const string ArchitectureCacheKey = "ffmpeg.hardware.nvidia.architecture";
+    private const string ModelCacheKey = "ffmpeg.hardware.nvidia.model";
     private readonly ILogger<HardwareCapabilitiesFactory> _logger;
 
     private readonly IMemoryCache _memoryCache;
@@ -31,9 +32,10 @@ public class HardwareCapabilitiesFactory : IHardwareCapabilitiesFactory
 
     private async Task<IHardwareCapabilities> GetNvidiaCapabilities(string ffmpegPath)
     {
-        if (_memoryCache.TryGetValue(CacheKey, out int cachedArchitecture))
+        if (_memoryCache.TryGetValue(ArchitectureCacheKey, out int cachedArchitecture)
+            && _memoryCache.TryGetValue(ModelCacheKey, out string cachedModel))
         {
-            return new NvidiaHardwareCapabilities(cachedArchitecture);
+            return new NvidiaHardwareCapabilities(cachedArchitecture, cachedModel);
         }
 
         string[] arguments =
@@ -57,13 +59,20 @@ public class HardwareCapabilitiesFactory : IHardwareCapabilitiesFactory
         Option<string> maybeLine = Optional(output.Split("\n").FirstOrDefault(x => x.Contains("GPU")));
         foreach (string line in maybeLine)
         {
-            const string PATTERN = @"SM\s+(\d\.\d)";
-            Match match = Regex.Match(line, PATTERN);
+            const string ARCHITECTURE_PATTERN = @"SM\s+(\d\.\d)";
+            Match match = Regex.Match(line, ARCHITECTURE_PATTERN);
             if (match.Success && int.TryParse(match.Groups[1].Value.Replace(".", string.Empty), out int architecture))
             {
-                _logger.LogInformation("Detected NVIDIA GPU architecture SM {Architecture}", architecture);
-                _memoryCache.Set(CacheKey, architecture);
-                return new NvidiaHardwareCapabilities(architecture);
+                const string MODEL_PATTERN = @"(GTX\s+[0-9a-zA-Z]+[\sTtIi]+)";
+                Match modelMatch = Regex.Match(line, MODEL_PATTERN);
+                string model = modelMatch.Success ? modelMatch.Groups[1].Value.Trim() : "unknown";
+                _logger.LogInformation(
+                    "Detected NVIDIA GPU model {Model} architecture SM {Architecture}",
+                    model,
+                    architecture);
+                _memoryCache.Set(ArchitectureCacheKey, architecture);
+                _memoryCache.Set(ModelCacheKey, model);
+                return new NvidiaHardwareCapabilities(architecture, model);
             }
         }
 
