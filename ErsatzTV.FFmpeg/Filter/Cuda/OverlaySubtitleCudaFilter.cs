4710262306c97f12bdbf8ba1ca2016ef0c511c9b
Jason Dove <jason@jasondove.me>
2022-09-18 09:37:00 -0500

    fix image subtitle scaling

--- ErsatzTV.FFmpeg/Filter/Cuda/OverlaySubtitleCudaFilter.cs
+++ ErsatzTV.FFmpeg/Filter/Cuda/OverlaySubtitleCudaFilter.cs
@@ -2,6 +2,6 @@
 
 public class OverlaySubtitleCudaFilter : BaseFilter
 {
-    public override string Filter => "overlay_cuda";
+    public override string Filter => "overlay_cuda=x=(W-w)/2:y=(H-h)/2";
     public override FrameState NextState(FrameState currentState) => currentState;
 }
