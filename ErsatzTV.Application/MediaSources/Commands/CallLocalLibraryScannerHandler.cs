995dddbf639f6f43fa2061c36bd1d78aa0ba928a
Jason Dove <jason@jasondove.me>
2022-12-29 21:24:12 -0600

    rework scanner handlers

--- ErsatzTV.Application/MediaSources/Commands/CallLocalLibraryScannerHandler.cs
+++ ErsatzTV.Application/MediaSources/Commands/CallLocalLibraryScannerHandler.cs
@@ -1,34 +1,20 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Threading.Channels;
-using CliWrap;
-using ErsatzTV.Application.Search;
+﻿using System.Threading.Channels;
+using ErsatzTV.Application.Libraries;
 using ErsatzTV.Core;
-using ErsatzTV.Core.MediaSources;
-using ErsatzTV.Core.Metadata;
 using ErsatzTV.FFmpeg.Runtime;
-using Newtonsoft.Json;
-using Serilog;
-using Serilog.Formatting.Compact.Reader;
 
 namespace ErsatzTV.Application.MediaSources;
 
-public class CallLocalLibraryScannerHandler : IRequestHandler<ForceScanLocalLibrary, Either<BaseError, string>>,
+public class CallLocalLibraryScannerHandler : CallLibraryScannerHandler,
+    IRequestHandler<ForceScanLocalLibrary, Either<BaseError, string>>,
     IRequestHandler<ScanLocalLibraryIfNeeded, Either<BaseError, string>>
 {
-    private readonly ChannelWriter<ISearchIndexBackgroundServiceRequest> _channel;
-    private readonly IMediator _mediator;
-    private readonly IRuntimeInfo _runtimeInfo;
-    private string _libraryName;
-
     public CallLocalLibraryScannerHandler(
         ChannelWriter<ISearchIndexBackgroundServiceRequest> channel,
         IMediator mediator,
         IRuntimeInfo runtimeInfo)
+        : base(channel, mediator, runtimeInfo)
     {
-        _channel = channel;
-        _mediator = mediator;
-        _runtimeInfo = runtimeInfo;
     }
 
     Task<Either<BaseError, string>> IRequestHandler<ForceScanLocalLibrary, Either<BaseError, string>>.Handle(
@@ -52,119 +38,17 @@ public class CallLocalLibraryScannerHandler : IRequestHandler<ForceScanLocalLibr
         IScanLocalLibrary request,
         CancellationToken cancellationToken)
     {
-        try
+        var arguments = new List<string>
         {
-            var arguments = new List<string>
-            {
-                "--local",
-                request.LibraryId.ToString()
-            };
+            "--local",
+            request.LibraryId.ToString()
+        };
 
-            if (request.ForceScan)
-            {
-                arguments.Add("--force");
-            }
-
-            using var forcefulCts = new CancellationTokenSource();
-
-            await using CancellationTokenRegistration link = cancellationToken.Register(
-                () => forcefulCts.CancelAfter(TimeSpan.FromSeconds(10))
-            );
-
-            CommandResult process = await Cli.Wrap(scanner)
-                .WithArguments(arguments)
-                .WithValidation(CommandResultValidation.None)
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(ProcessLogOutput))
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(ProcessProgressOutput))
-                .ExecuteAsync(forcefulCts.Token, cancellationToken);
-
-            if (process.ExitCode != 0)
-            {
-                return BaseError.New($"ErsatzTV.Scanner exited with code {process.ExitCode}");
-            }
-        }
-        catch (OperationCanceledException)
+        if (request.ForceScan)
         {
-            // do nothing
+            arguments.Add("--force");
         }
 
-        return _libraryName ?? string.Empty;
-    }
-
-    private static void ProcessLogOutput(string s)
-    {
-        if (!string.IsNullOrWhiteSpace(s))
-        {
-            try
-            {
-                Log.Write(LogEventReader.ReadFromString(s));
-            }
-            catch
-            {
-                Console.WriteLine(s);
-            }
-        }
-    }
-
-    private async Task ProcessProgressOutput(string s)
-    {
-        if (!string.IsNullOrWhiteSpace(s))
-        {
-            try
-            {
-                ScannerProgressUpdate progressUpdate = JsonConvert.DeserializeObject<ScannerProgressUpdate>(s);
-                if (progressUpdate != null)
-                {
-                    if (!string.IsNullOrWhiteSpace(progressUpdate.LibraryName))
-                    {
-                        _libraryName = progressUpdate.LibraryName;
-                    }
-
-                    if (progressUpdate.PercentComplete is not null)
-                    {
-                        var progress = new LibraryScanProgress(
-                            progressUpdate.LibraryId,
-                            progressUpdate.PercentComplete.Value);
-
-                        await _mediator.Publish(progress);
-                    }
-
-                    if (progressUpdate.ItemsToReindex.Length > 0)
-                    {
-                        var reindex = new ReindexMediaItems(progressUpdate.ItemsToReindex);
-                        await _channel.WriteAsync(reindex);
-                    }
-
-                    if (progressUpdate.ItemsToRemove.Length > 0)
-                    {
-                        var remove = new RemoveMediaItems(progressUpdate.ItemsToRemove);
-                        await _channel.WriteAsync(remove);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Logger.Warning(ex, "Unable to process scanner progress update");
-            }
-        }
-    }
-
-    private Validation<BaseError, string> Validate()
-    {
-        string executable = _runtimeInfo.IsOSPlatform(OSPlatform.Windows)
-            ? "ErsatzTV.Scanner.exe"
-            : "ErsatzTV.Scanner";
-        
-        string processFileName = Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty;
-        if (!string.IsNullOrWhiteSpace(processFileName))
-        {
-            string localFileName = Path.Combine(Path.GetDirectoryName(processFileName) ?? string.Empty, executable);
-            if (File.Exists(localFileName))
-            {
-                return localFileName;
-            }
-        }
-
-        return BaseError.New("Unable to locate ErsatzTV.Scanner executable");
+        return await base.PerformScan(scanner, arguments, cancellationToken);
     }
 }
--- ErsatzTV.Application/Plex/Commands/CallPlexLibraryScannerHandler.cs
+++ ErsatzTV.Application/Plex/Commands/CallPlexLibraryScannerHandler.cs
@@ -1,25 +1,20 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
-using CliWrap;
+﻿using System.Threading.Channels;
+using ErsatzTV.Application.Libraries;
 using ErsatzTV.Core;
-using ErsatzTV.Core.Metadata;
 using ErsatzTV.FFmpeg.Runtime;
-using Newtonsoft.Json;
-using Serilog;
-using Serilog.Formatting.Compact.Reader;
 
 namespace ErsatzTV.Application.Plex;
 
-public class CallPlexLibraryScannerHandler : IRequestHandler<ForceSynchronizePlexLibraryById, Either<BaseError, string>>,
+public class CallPlexLibraryScannerHandler : CallLibraryScannerHandler,
+    IRequestHandler<ForceSynchronizePlexLibraryById, Either<BaseError, string>>,
     IRequestHandler<SynchronizePlexLibraryByIdIfNeeded, Either<BaseError, string>>
 {
-    private readonly IMediator _mediator;
-    private readonly IRuntimeInfo _runtimeInfo;
-
-    public CallPlexLibraryScannerHandler(IMediator mediator, IRuntimeInfo runtimeInfo)
+    public CallPlexLibraryScannerHandler(
+        ChannelWriter<ISearchIndexBackgroundServiceRequest> channel,
+        IMediator mediator,
+        IRuntimeInfo runtimeInfo)
+        : base(channel, mediator, runtimeInfo)
     {
-        _mediator = mediator;
-        _runtimeInfo = runtimeInfo;
     }
 
     Task<Either<BaseError, string>> IRequestHandler<ForceSynchronizePlexLibraryById, Either<BaseError, string>>.Handle(
@@ -30,7 +25,9 @@ public class CallPlexLibraryScannerHandler : IRequestHandler<ForceSynchronizePle
         SynchronizePlexLibraryByIdIfNeeded request,
         CancellationToken cancellationToken) => Handle(request, cancellationToken);
 
-    private async Task<Either<BaseError, string>> Handle(ISynchronizePlexLibraryById request, CancellationToken cancellationToken)
+    private async Task<Either<BaseError, string>> Handle(
+        ISynchronizePlexLibraryById request,
+        CancellationToken cancellationToken)
     {
         Validation<BaseError, string> validation = Validate();
         return await validation.Match(
@@ -43,98 +40,22 @@ public class CallPlexLibraryScannerHandler : IRequestHandler<ForceSynchronizePle
         ISynchronizePlexLibraryById request,
         CancellationToken cancellationToken)
     {
-        try
+        var arguments = new List<string>
         {
-            var arguments = new List<string>
-            {
-                "--plex",
-                request.PlexLibraryId.ToString()
-            };
+            "--plex",
+            request.PlexLibraryId.ToString()
+        };
 
-            if (request.ForceScan)
-            {
-                arguments.Add("--force");
-            }
-
-            if (request.DeepScan)
-            {
-                arguments.Add("--deep");
-            }
-
-            using var forcefulCts = new CancellationTokenSource();
-
-            await using CancellationTokenRegistration link = cancellationToken.Register(
-                () => forcefulCts.CancelAfter(TimeSpan.FromSeconds(10))
-            );
-
-            CommandResult process = await Cli.Wrap(scanner)
-                .WithArguments(arguments)
-                .WithValidation(CommandResultValidation.None)
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(ProcessLogOutput))
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(ProcessProgressOutput))
-                .ExecuteAsync(forcefulCts.Token, cancellationToken);
-
-            if (process.ExitCode != 0)
-            {
-                return BaseError.New($"ErsatzTV.Scanner exited with code {process.ExitCode}");
-            }
-
-            // TODO: return plex library name?
-            return string.Empty;
-        }
-        catch (OperationCanceledException)
+        if (request.ForceScan)
         {
-            return string.Empty;
-        }
-    }
-
-    private static void ProcessLogOutput(string s)
-    {
-        if (!string.IsNullOrWhiteSpace(s))
-        {
-            try
-            {
-                Log.Write(LogEventReader.ReadFromString(s));
-            }
-            catch
-            {
-                Console.WriteLine(s);
-            }
-        }
-    }
-
-    private async Task ProcessProgressOutput(string s)
-    {
-        if (!string.IsNullOrWhiteSpace(s))
-        {
-            try
-            {
-                LibraryScanProgress libraryScanProgress = JsonConvert.DeserializeObject<LibraryScanProgress>(s);
-                await _mediator.Publish(libraryScanProgress);
-            }
-            catch
-            {
-                // do nothing
-            }
-        }
-    }
-
-    private Validation<BaseError, string> Validate()
-    {
-        string executable = _runtimeInfo.IsOSPlatform(OSPlatform.Windows)
-            ? "ErsatzTV.Scanner.exe"
-            : "ErsatzTV.Scanner";
-        
-        string processFileName = Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty;
-        if (!string.IsNullOrWhiteSpace(processFileName))
-        {
-            string localFileName = Path.Combine(Path.GetDirectoryName(processFileName) ?? string.Empty, executable);
-            if (File.Exists(localFileName))
-            {
-                return localFileName;
-            }
+            arguments.Add("--force");
         }
 
-        return BaseError.New("Unable to locate ErsatzTV.Scanner executable");
+        if (request.DeepScan)
+        {
+            arguments.Add("--deep");
+        }
+
+        return await base.PerformScan(scanner, arguments, cancellationToken);
     }
 }
