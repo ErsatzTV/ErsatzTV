5b716f23828258abaf2eb302921b18cb7c4cf63d
Jason Dove <jason@jasondove.me>
2023-01-01 08:49:15 -0600

    fix search bug (#1086)
    * fix removing media items from search index
    
    * update changelog

--- ErsatzTV.Application/Search/Commands/RemoveMediaItemsHandler.cs
+++ ErsatzTV.Application/Search/Commands/RemoveMediaItemsHandler.cs
@@ -2,13 +2,13 @@ using ErsatzTV.Core.Interfaces.Search;
 
 namespace ErsatzTV.Application.Search;
 
-public class RemoveMediaItemsHandler : IRequestHandler<ReindexMediaItems, Unit>
+public class RemoveMediaItemsHandler : IRequestHandler<RemoveMediaItems, Unit>
 {
     private readonly ISearchIndex _searchIndex;
 
     public RemoveMediaItemsHandler(ISearchIndex searchIndex) => _searchIndex = searchIndex;
 
-    public async Task<Unit> Handle(ReindexMediaItems request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(RemoveMediaItems request, CancellationToken cancellationToken)
     {
         await _searchIndex.RemoveItems(request.MediaItemIds);
         _searchIndex.Commit();
