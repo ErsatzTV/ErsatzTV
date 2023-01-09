79a8ea4b16d5c6996ee747fe9d7729180bbf1993
Jason Dove <jason@jasondove.me>
2023-01-08 20:26:50 -0600

    edit days of the week

---
+++ ErsatzTV/Validators/PlayoutAlternateScheduleEditViewModelValidator.cs
@@ -0,0 +1,12 @@
+using ErsatzTV.ViewModels;
+using FluentValidation;
+
+namespace ErsatzTV.Validators;
+
+public class PlayoutAlternateScheduleEditViewModelValidator : AbstractValidator<PlayoutAlternateScheduleEditViewModel>
+{
+    public PlayoutAlternateScheduleEditViewModelValidator()
+    {
+        RuleFor(p => p.ProgramSchedule).NotNull();
+    }
+}
