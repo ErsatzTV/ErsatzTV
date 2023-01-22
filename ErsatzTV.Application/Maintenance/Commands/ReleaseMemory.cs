aca6bfb0bbdea8ba8efe437b29db6ae1b3681bdb
Jason Dove <jason@jasondove.me>
2023-01-22 13:10:13 -0600

    fix multiple gcs after extracting subtitles (#1129)

--- ErsatzTV.Application/Maintenance/Commands/ReleaseMemory.cs
+++ ErsatzTV.Application/Maintenance/Commands/ReleaseMemory.cs
@@ -1,3 +1,6 @@
 namespace ErsatzTV.Application.Maintenance;
 
-public record ReleaseMemory(bool ForceAggressive) : IRequest<Unit>, IBackgroundServiceRequest;
+public record ReleaseMemory(bool ForceAggressive) : IRequest<Unit>, IBackgroundServiceRequest
+{
+    public DateTimeOffset RequestTime = DateTimeOffset.Now;
+}
