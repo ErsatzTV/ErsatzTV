f828c9cd29162ea6fa2675ca26bd80284794cddb
Jason Dove <jason@jasondove.me>
2023-01-25 09:29:50 -0600

    add oidc logout url to support auth0

--- ErsatzTV/OidcHelper.cs
+++ ErsatzTV/OidcHelper.cs
@@ -7,6 +7,7 @@ public static class OidcHelper
         Authority = configuration["OIDC:Authority"];
         ClientId = configuration["OIDC:ClientId"];
         ClientSecret = configuration["OIDC:ClientSecret"];
+        LogoutUri = configuration["OIDC:LogoutUri"];
 
         IsEnabled = !string.IsNullOrWhiteSpace(Authority) &&
                    !string.IsNullOrWhiteSpace(ClientId) &&
@@ -16,5 +17,6 @@ public static class OidcHelper
     public static string Authority { get; private set; }
     public static string ClientId { get; private set; }
     public static string ClientSecret { get; private set; }
+    public static string LogoutUri { get; private set; }
     public static bool IsEnabled { get; private set; }
 }
