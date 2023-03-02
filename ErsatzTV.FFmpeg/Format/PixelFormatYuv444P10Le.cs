457b45a9ea6e33882026468febee2cc91aa8e915
Jason Dove <jason@jasondove.me>
2023-03-01 19:56:14 -0600

    fix yuv444p10le (#1186)

--- ErsatzTV.FFmpeg/Format/PixelFormatYuv444P10Le.cs
+++ ErsatzTV.FFmpeg/Format/PixelFormatYuv444P10Le.cs
@@ -4,4 +4,5 @@ public class PixelFormatYuv444P10Le : IPixelFormat
 {
     public string Name => PixelFormat.YUV444P10LE;
     public string FFmpegName => FFmpegFormat.P010LE;
+    public int BitDepth => 10;
 }
