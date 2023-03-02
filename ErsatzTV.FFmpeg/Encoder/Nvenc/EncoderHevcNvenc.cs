0cd0632756a5927738a4ac6152fd7ed2fbc894e7
Jason Dove <jason@jasondove.me>
2023-03-01 20:17:58 -0600

    fix hevc_nvenc encoder on sm < 75 (#1187)

--- ErsatzTV.FFmpeg/Encoder/Nvenc/EncoderHevcNvenc.cs
+++ ErsatzTV.FFmpeg/Encoder/Nvenc/EncoderHevcNvenc.cs
@@ -1,13 +1,27 @@
-﻿using ErsatzTV.FFmpeg.Format;
+﻿using ErsatzTV.FFmpeg.Capabilities;
+using ErsatzTV.FFmpeg.Format;
 
 namespace ErsatzTV.FFmpeg.Encoder.Nvenc;
 
 public class EncoderHevcNvenc : EncoderBase
 {
+    private readonly bool _bFrames;
+    
+    public EncoderHevcNvenc(IHardwareCapabilities hardwareCapabilities)
+    {
+        if (hardwareCapabilities is NvidiaHardwareCapabilities nvidia)
+        {
+            _bFrames = nvidia.HevcBFrames;
+        }
+    }
+
     public override string Name => "hevc_nvenc";
     public override StreamKind Kind => StreamKind.Video;
     public override FrameState NextState(FrameState currentState) => currentState with
     {
         VideoFormat = VideoFormat.Hevc
     };
+
+    public override IList<string> OutputOptions =>
+        new[] { "-c:v", "hevc_nvenc", "-b_ref_mode", _bFrames ? "1" : "0" };
 }
