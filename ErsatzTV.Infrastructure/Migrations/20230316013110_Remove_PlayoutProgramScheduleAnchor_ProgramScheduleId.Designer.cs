b5839d0dc5951fbdc7e64eec197173eb32d1e084
Jason Dove <jason@jasondove.me>
2023-03-15 20:34:25 -0500

    maintain collection progress across alternate schedules (#1211)

--- ErsatzTV.Infrastructure/Migrations/20230316013110_Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId.Designer.cs
+++ ErsatzTV.Infrastructure/Migrations/20230316013110_Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ErsatzTV.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ErsatzTV.Infrastructure.Migrations
 {
     [DbContext(typeof(TvContext))]
-    partial class TvContextModelSnapshot : ModelSnapshot
+    [Migration("20230316013110_Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId")]
+    partial class Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
---
+++ ErsatzTV.Infrastructure/Migrations/20230316013110_Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_PlayoutProgramScheduleAnchor_ProgramScheduleId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayoutProgramScheduleAnchor_ProgramSchedule_ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlayoutProgramScheduleAnchor_ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor");
+
+            migrationBuilder.DropColumn(
+                name: "ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayoutProgramScheduleAnchor_ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor",
+                column: "ProgramScheduleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayoutProgramScheduleAnchor_ProgramSchedule_ProgramScheduleId",
+                table: "PlayoutProgramScheduleAnchor",
+                column: "ProgramScheduleId",
+                principalTable: "ProgramSchedule",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
