b7bfd6462e195f61b7e9fd4bd09cf9a33ecd2255
Jason Dove <jason@jasondove.me>
2023-03-16 20:12:08 -0500

    formatting and minor refactoring

--- ErsatzTV/ConditionalAuthorizeFilter.cs
+++ ErsatzTV/ConditionalAuthorizeFilter.cs
@@ -1,25 +1,19 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace ErsatzTV
+namespace ErsatzTV;
+
+public class ConditionalAuthorizeFilter : AuthorizeFilter
 {
-    public class ConditionalAuthorizeFilter : AuthorizeFilter
+    public ConditionalAuthorizeFilter(string policy) : base(
+        new AuthorizationPolicyBuilder().RequireAuthenticatedUser().AddAuthenticationSchemes("jwt")
+            .RequireAssertion(_ => JwtHelper.IsEnabled).Build())
     {
-        public ConditionalAuthorizeFilter(string policy) : base(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().AddAuthenticationSchemes("jwt").RequireAssertion(_ => JwtHelper.IsEnabled).Build())
-        {
-        }
-
-        public override Task OnAuthorizationAsync(AuthorizationFilterContext context)
-        {
-            if (JwtHelper.IsEnabled)
-            {
-                return base.OnAuthorizationAsync(context);
-            }
-
-            return Task.CompletedTask;
-        }
     }
 
+    public override Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        return JwtHelper.IsEnabled ? base.OnAuthorizationAsync(context) : Task.CompletedTask;
+    }
 }
