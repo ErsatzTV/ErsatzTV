6b3600529712e38a451e388b401791ec1be1632b
Jason Dove <jason@jasondove.me>
2023-04-07 13:22:19 -0500

    rescan all media server libraries

--- ErsatzTV.Infrastructure/Migrations/20230407181640_Rescan_AllMediaServerLibraries_Subtitles.Designer.cs
+++ ErsatzTV.Infrastructure/Migrations/20230407181640_Rescan_AllMediaServerLibraries_Subtitles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ErsatzTV.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ErsatzTV.Infrastructure.Migrations
 {
     [DbContext(typeof(TvContext))]
-    partial class TvContextModelSnapshot : ModelSnapshot
+    [Migration("20230407181640_Rescan_AllMediaServerLibraries_Subtitles")]
+    partial class Rescan_AllMediaServerLibraries_Subtitles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.4");
