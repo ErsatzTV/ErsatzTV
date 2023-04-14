494142f0260973116cf643dcfe1f8af0c8bd838a
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-04-14 15:09:12 -0500

    fix media server text subtitle extraction (#1246)

---
+++ ErsatzTV.Infrastructure/Migrations/20230414193046_Clear_SubtitleIsExtracted_AllMediaServers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Clear_SubtitleIsExtracted_AllMediaServers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+update Subtitle set IsExtracted = 0 where Id in
+  (select Subtitle.Id
+         from Subtitle
+                  inner join EpisodeMetadata EM on Subtitle.EpisodeMetadataId = EM.Id
+                  inner join MediaItem MI on EM.EpisodeId = MI.Id
+                  inner join LibraryPath LP on MI.LibraryPathId = LP.Id
+                  inner join PlexLibrary PL on PL.Id = LP.LibraryId
+         where Subtitle.Codec = 'srt'
+           and IsExtracted = 1)");
+
+            migrationBuilder.Sql(
+                @"
+update Subtitle set IsExtracted = 0 where Id in
+  (select Subtitle.Id
+         from Subtitle
+                  inner join EpisodeMetadata EM on Subtitle.EpisodeMetadataId = EM.Id
+                  inner join MediaItem MI on EM.EpisodeId = MI.Id
+                  inner join LibraryPath LP on MI.LibraryPathId = LP.Id
+                  inner join EmbyLibrary EL on EL.Id = LP.LibraryId
+         where Subtitle.Codec = 'srt'
+           and IsExtracted = 1)");
+
+            migrationBuilder.Sql(
+                @"
+update Subtitle set IsExtracted = 0 where Id in
+  (select Subtitle.Id
+         from Subtitle
+                  inner join EpisodeMetadata EM on Subtitle.EpisodeMetadataId = EM.Id
+                  inner join MediaItem MI on EM.EpisodeId = MI.Id
+                  inner join LibraryPath LP on MI.LibraryPathId = LP.Id
+                  inner join JellyfinLibrary JL on JL.Id = LP.LibraryId
+         where Subtitle.Codec = 'srt'
+           and IsExtracted = 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
