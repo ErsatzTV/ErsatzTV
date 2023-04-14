b89deffda3dcae176d6f1b7567622ad64d496a62
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-04-14 12:53:33 -0500

    cleanup delete channel handler syntax (#1245)

--- ErsatzTV.Core/Interfaces/Repositories/IChannelRepository.cs
+++ ErsatzTV.Core/Interfaces/Repositories/IChannelRepository.cs
@@ -7,6 +7,4 @@ public interface IChannelRepository
     Task<Option<Channel>> Get(int id);
     Task<Option<Channel>> GetByNumber(string number);
     Task<List<Channel>> GetAll();
-    Task<List<Channel>> GetAllForGuide();
-    Task Delete(int channelId);
 }
