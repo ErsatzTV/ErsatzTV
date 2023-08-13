77b32b0f0903b3413df31f274b2b5e9a4d57d59a
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-08-13 09:59:48 -0500

    fix github repo links (#1378)

--- ErsatzTV.Infrastructure/GitHub/IGitHubApi.cs
+++ ErsatzTV.Infrastructure/GitHub/IGitHubApi.cs
@@ -3,12 +3,12 @@ using Refit;
 
 namespace ErsatzTV.Infrastructure.GitHub;
 
-[Headers("Accept: application/vnd.github.v3+json", "User-Agent: jasongdove/ErsatzTV")]
+[Headers("Accept: application/vnd.github.v3+json", "User-Agent: ErsatzTV/ErsatzTV")]
 public interface IGitHubApi
 {
-    [Get("/repos/jasongdove/ErsatzTV/releases")]
+    [Get("/repos/ErsatzTV/ErsatzTV/releases")]
     public Task<List<GitHubTag>> GetReleases(CancellationToken cancellationToken);
 
-    [Get("/repos/jasongdove/ErsatzTV/releases/tags/{tag}")]
+    [Get("/repos/ErsatzTV/ErsatzTV/releases/tags/{tag}")]
     public Task<GitHubTag> GetTag(string tag, CancellationToken cancellationToken);
 }
