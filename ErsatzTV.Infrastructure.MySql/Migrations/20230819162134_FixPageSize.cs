b43271b3ac263135c41dd8434c2fa616a49556c0
Jason Dove <jason@jasondove.me>
2023-08-19 11:25:03 -0500

    fix mysql saved page size

---
+++ ErsatzTV.Infrastructure.MySql/Migrations/20230819162134_FixPageSize.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPageSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE ConfigElement SET Value = '10' WHERE `Key` LIKE '%page_size%' AND Value = '0'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
