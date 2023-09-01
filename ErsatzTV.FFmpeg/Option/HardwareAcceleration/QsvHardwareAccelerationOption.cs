8b0b927a5c4080a37302e6d3337962b054adb7d1
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-09-01 11:50:56 -0500

    use d3d11va for qsv accel on windows (#1408)

--- ErsatzTV.FFmpeg/Option/HardwareAcceleration/QsvHardwareAccelerationOption.cs
+++ ErsatzTV.FFmpeg/Option/HardwareAcceleration/QsvHardwareAccelerationOption.cs
@@ -20,7 +20,7 @@ public class QsvHardwareAccelerationOption : GlobalOption
         get
         {
             string[] initDevices = OperatingSystem.IsWindows()
-                ? new[] { "-init_hw_device", "qsv=hw:hw,child_device_type=dxva2", "-filter_hw_device", "hw" }
+                ? new[] { "-init_hw_device", "d3d11va=hw:,vendor=0x8086", "-filter_hw_device", "hw" }
                 : new[] { "-init_hw_device", "qsv=hw", "-filter_hw_device", "hw" };
 
             var result = new List<string>
