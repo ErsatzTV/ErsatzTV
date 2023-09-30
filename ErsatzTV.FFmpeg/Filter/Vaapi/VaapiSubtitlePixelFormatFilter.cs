46bf9ef990a065e84099f0a9d0eaaa005a6480fa
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-09-30 13:10:23 -0500

    fix intel vaapi pgs subtitle pixel format (#1455)

---
+++ ErsatzTV.FFmpeg/Filter/Vaapi/VaapiSubtitlePixelFormatFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace ErsatzTV.FFmpeg.Filter.Vaapi;
+
+public class VaapiSubtitlePixelFormatFilter : BaseFilter
+{
+    public override FrameState NextState(FrameState currentState) => currentState;
+
+    public override string Filter => "format=vaapi|yuva420p|yuva444p|yuva422p|rgba|abgr|bgra|gbrap|ya8";
+}
