e2a516f5e8bf0c586e243675706e5d4cc32f6cea
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-01-09 05:26:57 -0600

    fix external json playouts with mysql (#1542)

---
+++ ErsatzTV.Infrastructure.MySql/Migrations/20240109112430_AddExternalJsonPlayout.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalJsonPlayout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramScheduleId",
+                table: "Playout",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalJsonFile",
+                table: "Playout",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalJsonFile",
+                table: "Playout");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramScheduleId",
+                table: "Playout",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
