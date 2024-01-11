e7dd0ddfde2256599d85e5332de793f7b107b1bc
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-01-11 09:08:09 -0600

    disambiguate seasons

--- ErsatzTV.Application/Search/Queries/SearchTelevisionSeasonsHandler.cs
+++ ErsatzTV.Application/Search/Queries/SearchTelevisionSeasonsHandler.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using ErsatzTV.Application.MediaItems;
 using ErsatzTV.Core.Domain;
 using ErsatzTV.Infrastructure.Data;
@@ -25,21 +26,25 @@ public class SearchTelevisionSeasonsHandler : IRequestHandler<SearchTelevisionSe
                 where EF.Functions.Like(showMetadata.Title + " " + seasonMetadata.Title, $"%{request.Query}%")
                 orderby EF.Functions.Collate(showMetadata.Title, TvContext.CaseInsensitiveCollation), season
                     .SeasonNumber
-                select new TelevisionSeason(season.Id, showMetadata.Title, season.SeasonNumber))
+                select new TelevisionSeason(season.Id, showMetadata.Title, showMetadata.Year, season.SeasonNumber))
             .Take(20)
             .ToListAsync(cancellationToken)
             .Map(list => list.Map(ToNamedMediaItem).ToList());
     }
 
-    private static NamedMediaItemViewModel ToNamedMediaItem(TelevisionSeason season) => new(
-        season.Id,
-        $"{ShowTitle(season)} ({SeasonTitle(season)})");
+    private static NamedMediaItemViewModel ToNamedMediaItem(TelevisionSeason season) =>
+        new(season.Id, $"{ShowTitle(season)} - {SeasonTitle(season)}");
 
-    private static string ShowTitle(TelevisionSeason season) => $"{season.Title ?? "???"}";
+    private static string ShowTitle(TelevisionSeason season)
+    {
+        string title = season.Title ?? "???";
+        string year = season.Year.HasValue ? season.Year.Value.ToString(CultureInfo.InvariantCulture) : "???";
+        return $"{title} ({year})";
+    }
 
     private static string SeasonTitle(TelevisionSeason season) => season.SeasonNumber == 0
         ? "Specials"
         : $"Season {season.SeasonNumber}";
 
-    public record TelevisionSeason(int Id, string Title, int SeasonNumber);
+    public record TelevisionSeason(int Id, string Title, int? Year, int SeasonNumber);
 }
