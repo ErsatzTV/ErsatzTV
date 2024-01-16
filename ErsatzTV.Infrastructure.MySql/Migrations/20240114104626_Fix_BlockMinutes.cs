9b2762bca647cc730587539d0e8c338034c0c7ee
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-01-16 05:39:03 -0600

    fix mysql migration

--- ErsatzTV.Infrastructure.MySql/Migrations/20240114104626_Fix_BlockMinutes.cs
+++ ErsatzTV.Infrastructure.MySql/Migrations/20240114104626_Fix_BlockMinutes.cs
@@ -10,7 +10,7 @@ namespace ErsatzTV.Infrastructure.MySql.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"UPDATE Block SET Minutes = CAST(CEILING(Minutes / 15.0) * 15 AS INT)");
+            migrationBuilder.Sql(@"UPDATE Block SET Minutes = CAST(CEILING(Minutes / 15.0) * 15 AS UNSIGNED)");
         }
 
         /// <inheritdoc />
