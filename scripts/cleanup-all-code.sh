15d4b0f82bdb1896ffe49b466e79c45856db7ac8
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-01-16 13:28:46 -0600

    remove v2 ui and node (#1564)

--- scripts/cleanup-all-code.sh
+++ scripts/cleanup-all-code.sh
@@ -3,4 +3,4 @@
 cd "$(git rev-parse --show-cdup)" || exit
 
 dotnet tool restore
-dotnet jb cleanupcode ErsatzTV.sln --exclude='CHANGELOG.md;scripts/**;generated/**;ErsatzTV/client-app/**'
+dotnet jb cleanupcode ErsatzTV.sln --exclude='CHANGELOG.md;scripts/**;generated/**'
