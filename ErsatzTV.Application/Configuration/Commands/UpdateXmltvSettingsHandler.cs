3b4c9935301014a5ea3615c8ba768ec5cfe5f839
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-02-21 12:46:09 -0600

    add xmltv time zone option (#1624)

---
+++ ErsatzTV.Application/Configuration/Commands/UpdateXmltvSettingsHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+using ErsatzTV.Application.Channels;
+using ErsatzTV.Core;
+using ErsatzTV.Core.Domain;
+using ErsatzTV.Core.Interfaces.Repositories;
+using ErsatzTV.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErsatzTV.Application.Configuration;
+
+public class UpdateXmltvSettingsHandler(
+    IDbContextFactory<TvContext> dbContextFactory,
+    IConfigElementRepository configElementRepository,
+    ChannelWriter<IBackgroundServiceRequest> workerChannel)
+    : IRequestHandler<UpdateXmltvSettings, Either<BaseError, Unit>>
+{
+    public async Task<Either<BaseError, Unit>> Handle(
+        UpdateXmltvSettings request,
+        CancellationToken cancellationToken) => await ApplyUpdate(request.XmltvSettings);
+
+    private async Task<Unit> ApplyUpdate(XmltvSettingsViewModel xmltvSettings)
+    {
+        await configElementRepository.Upsert(ConfigElementKey.XmltvTimeZone, xmltvSettings.TimeZone);
+
+        await using TvContext dbContext = await dbContextFactory.CreateDbContextAsync();
+
+        foreach (string channelNumber in await dbContext.Channels.Map(c => c.Number).ToListAsync())
+        {
+            await workerChannel.WriteAsync(new RefreshChannelData(channelNumber));
+        }
+
+        return Unit.Default;
+    }
+}
