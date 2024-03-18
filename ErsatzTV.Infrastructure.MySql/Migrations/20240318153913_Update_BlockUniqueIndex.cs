76eb54c67902ffd82514109679e56420fffe67e9
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-03-18 11:00:06 -0500

    manually fix mysql migration

---
+++ ErsatzTV.Infrastructure.MySql/Migrations/20240318153913_Update_BlockUniqueIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_BlockUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.DropIndex(
+            //     name: "IX_Block_BlockGroupId",
+            //     table: "Block");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Block_Name",
+                table: "Block");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Block_BlockGroupId_Name",
+                table: "Block",
+                columns: new[] { "BlockGroupId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Block_BlockGroupId_Name",
+                table: "Block");
+
+            // migrationBuilder.CreateIndex(
+            //     name: "IX_Block_BlockGroupId",
+            //     table: "Block",
+            //     column: "BlockGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Block_Name",
+                table: "Block",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
