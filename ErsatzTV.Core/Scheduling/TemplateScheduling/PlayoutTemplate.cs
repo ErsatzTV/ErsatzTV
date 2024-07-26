53bd7456781acaa3c3aab0b27fd308aef6c9d57d
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-07-26 13:10:27 -0500

    add playout template show content (#1812)

--- ErsatzTV.Core/Scheduling/TemplateScheduling/PlayoutTemplate.cs
+++ ErsatzTV.Core/Scheduling/TemplateScheduling/PlayoutTemplate.cs
@@ -2,6 +2,6 @@ namespace ErsatzTV.Core.Scheduling.TemplateScheduling;
 
 public class PlayoutTemplate
 {
-    public List<PlayoutTemplateContentSearchItem> Content { get; set; } = [];
+    public List<PlayoutTemplateContentItem> Content { get; set; } = [];
     public List<PlayoutTemplateItem> Playout { get; set; } = [];
 }
---
+++ ErsatzTV.Core/Scheduling/TemplateScheduling/PlayoutTemplateContentGuid.cs
@@ -0,0 +1,7 @@
+namespace ErsatzTV.Core.Scheduling.TemplateScheduling;
+
+public class PlayoutTemplateContentGuid
+{
+    public string Source { get; set; }
+    public string Value { get; set; }
+}
