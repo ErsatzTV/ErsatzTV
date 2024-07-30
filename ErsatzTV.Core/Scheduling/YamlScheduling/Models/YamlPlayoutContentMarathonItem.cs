a89c4090c7c943df91f7566bf2a2411c03de4108
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-07-30 18:21:00 -0500

    add basic marathon content

---
+++ ErsatzTV.Core/Scheduling/YamlScheduling/Models/YamlPlayoutContentMarathonItem.cs
@@ -0,0 +1,19 @@
+using YamlDotNet.Serialization;
+
+namespace ErsatzTV.Core.Scheduling.YamlScheduling.Models;
+
+public class YamlPlayoutContentMarathonItem : YamlPlayoutContentItem
+{
+    public string Marathon { get; set; }
+
+    public List<YamlPlayoutContentGuid> Guids { get; set; } = [];
+
+    [YamlMember(Alias = "group_by", ApplyNamingConventions = false)]
+    public string GroupBy { get; set; }
+
+    [YamlMember(Alias = "item_order", ApplyNamingConventions = false)]
+    public string ItemOrder { get; set; }
+
+    [YamlMember(Alias = "play_all_items", ApplyNamingConventions = false)]
+    public bool PlayAllItems { get; set; }
+}
