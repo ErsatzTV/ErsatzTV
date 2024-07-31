3982a0befa5ad4d24aa7244fb4fd6323233f6abc
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-07-31 16:12:49 -0500

    group music videos by album

---
+++ ErsatzTV.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+namespace ErsatzTV.Core.Extensions;
+
+public static class StringExtensions
+{
+    public static int GetStableHashCode(this string str)
+    {
+        unchecked
+        {
+            int hash1 = 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < str.Length && str[i] != '\0'; i += 2)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                if (i == str.Length - 1 || str[i + 1] == '\0')
+                    break;
+                hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
+    }
+}
