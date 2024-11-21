dc112f0c7db94bc29b545bad1ed3b3952ff4216f
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-11-20 22:27:09 -0600

    add default song album art; adjust default blurhashes (#1946)
    * fix song progress on white backgrounds
    
    * remove yellow from default song backgrounds
    
    * add default album art for generated song images
    
    * update changelog

--- ErsatzTV.Core/FFmpeg/SongVideoGenerator.cs
+++ ErsatzTV.Core/FFmpeg/SongVideoGenerator.cs
@@ -126,18 +126,15 @@ public class SongVideoGenerator : ISongVideoGenerator
             int leftMarginPercent = HORIZONTAL_MARGIN_PERCENT;
             int rightMarginPercent = HORIZONTAL_MARGIN_PERCENT;
 
-            if (metadata.Artwork.Any(a => a.ArtworkKind == ArtworkKind.Thumbnail))
+            switch (watermarkLocation)
             {
-                switch (watermarkLocation)
-                {
-                    case WatermarkLocation.BottomLeft:
-                        leftMarginPercent += WATERMARK_WIDTH_PERCENT + HORIZONTAL_MARGIN_PERCENT;
-                        break;
-                    case WatermarkLocation.BottomRight:
-                        leftMarginPercent = rightMarginPercent = HORIZONTAL_MARGIN_PERCENT;
-                        rightMarginPercent += WATERMARK_WIDTH_PERCENT + HORIZONTAL_MARGIN_PERCENT;
-                        break;
-                }
+                case WatermarkLocation.BottomLeft:
+                    leftMarginPercent += WATERMARK_WIDTH_PERCENT + HORIZONTAL_MARGIN_PERCENT;
+                    break;
+                case WatermarkLocation.BottomRight:
+                    leftMarginPercent = rightMarginPercent = HORIZONTAL_MARGIN_PERCENT;
+                    rightMarginPercent += WATERMARK_WIDTH_PERCENT + HORIZONTAL_MARGIN_PERCENT;
+                    break;
             }
 
             var leftMargin = (int)Math.Round(leftMarginPercent / 100.0 * channel.FFmpegProfile.Resolution.Width);
@@ -161,30 +158,40 @@ public class SongVideoGenerator : ISongVideoGenerator
                 .BuildFile();
 
             // use thumbnail (cover art) if present
-            foreach (Artwork artwork in Optional(
-                         metadata.Artwork.Find(a => a.ArtworkKind == ArtworkKind.Thumbnail)))
-            {
-                // signal that we want to use cover art as watermark
-                videoVersion = new CoverArtMediaVersion
-                {
-                    Chapters = new List<MediaChapter>(),
-                    // always stretch cover art
-                    Width = 192,
-                    Height = 108,
-                    SampleAspectRatio = "1:1",
-                    Streams = new List<MediaStream>
+            // fall back to default art
+            Artwork artwork = await Optional(metadata.Artwork.Find(a => a.ArtworkKind == ArtworkKind.Thumbnail))
+                .IfNoneAsync(
+                    new Artwork
                     {
-                        new() { MediaStreamKind = MediaStreamKind.Video, Index = 0 }
-                    }
-                };
+                        Id = 0,
+                        ArtworkKind = ArtworkKind.Thumbnail,
+                        Path = Path.Combine(FileSystemLayout.ResourcesCacheFolder, "song_album_cover_512.png"),
+                    });
 
-                string customPath = _imageCache.GetPathForImage(
-                    artwork.Path,
-                    ArtworkKind.Thumbnail,
-                    Option<int>.None);
+            // signal that we want to use cover art as watermark
+            videoVersion = new CoverArtMediaVersion
+            {
+                Chapters = [],
+                // always stretch cover art
+                Width = 192,
+                Height = 108,
+                SampleAspectRatio = "1:1",
+                Streams = new List<MediaStream>
+                {
+                    new() { MediaStreamKind = MediaStreamKind.Video, Index = 0 }
+                }
+            };
 
-                watermarkPath = customPath;
+            string customPath = _imageCache.GetPathForImage(
+                artwork.Path,
+                ArtworkKind.Thumbnail,
+                Option<int>.None);
 
+            watermarkPath = customPath;
+
+            // only blurhash real album art
+            if (artwork.Id > 0)
+            {
                 // randomize selected blur hash
                 var hashes = new List<string>
                 {
