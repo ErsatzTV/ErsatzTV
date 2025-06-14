80fa16fdaa1c95229bbeab6a80681cde91a650f2
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-06-14 08:35:41 -0500

    add pad_vaapi filter

---
+++ ErsatzTV.FFmpeg/Filter/Vaapi/PadVaapiFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace ErsatzTV.FFmpeg.Filter.Vaapi;
+
+public class PadVaapiFilter : BaseFilter
+{
+    private readonly FrameState _currentState;
+    private readonly FrameSize _paddedSize;
+
+    public PadVaapiFilter(FrameState currentState, FrameSize paddedSize)
+    {
+        _currentState = currentState;
+        _paddedSize = paddedSize;
+    }
+
+    public override string Filter
+    {
+        get
+        {
+            var pad = $"pad_vaapi=w={_paddedSize.Width}:h={_paddedSize.Height}:x=-1:y=-1:color=black";
+
+            return _currentState.FrameDataLocation == FrameDataLocation.Hardware
+                ? pad
+                : $"format=nv12|p010le|vaapi,hwupload,{pad}";
+        }
+    }
+
+    public override FrameState NextState(FrameState currentState) => currentState with
+    {
+        PaddedSize = _paddedSize,
+        FrameDataLocation = FrameDataLocation.Hardware
+    };
+}
