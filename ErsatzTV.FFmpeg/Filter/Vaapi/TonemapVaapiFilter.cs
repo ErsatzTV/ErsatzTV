fb2466d32dda70a1347307bfecc31b8d4cf892c4
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-07-07 20:58:45 +0000

    vaapi tonemap fixes (#2125)

--- ErsatzTV.FFmpeg/Filter/Vaapi/TonemapVaapiFilter.cs
+++ ErsatzTV.FFmpeg/Filter/Vaapi/TonemapVaapiFilter.cs
@@ -1,13 +1,16 @@
+using ErsatzTV.FFmpeg.Format;
+
 namespace ErsatzTV.FFmpeg.Filter.Vaapi;
 
 public class TonemapVaapiFilter(FFmpegState ffmpegState) : BaseFilter
 {
     public override string Filter =>
-        $"hwupload=derive_device=vaapi,hwmap=derive_device=opencl,tonemap_opencl=tonemap={ffmpegState.TonemapAlgorithm},hwmap=derive_device=vaapi:reverse=1";
+        $"hwupload=derive_device=vaapi,hwmap=derive_device=opencl,tonemap_opencl=tonemap={ffmpegState.TonemapAlgorithm},hwmap=derive_device=vaapi:reverse=1,scale_vaapi=format=p010le";
 
     public override FrameState NextState(FrameState currentState) =>
         currentState with
         {
-            FrameDataLocation = FrameDataLocation.Hardware
+            FrameDataLocation = FrameDataLocation.Hardware,
+            PixelFormat = new PixelFormatVaapi(PixelFormat.YUV420P10LE)
         };
 }
