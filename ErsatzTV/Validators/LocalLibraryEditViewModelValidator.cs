28a65e74bb6344cde7ef6b77aee51546e35c88f6
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-07-10 11:48:20 +0000

    use new form layout for local library editor (#2129)

--- ErsatzTV/Validators/LocalLibraryEditViewModelValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using ErsatzTV.ViewModels;
-using FluentValidation;
-
-namespace ErsatzTV.Validators;
-
-public class LocalLibraryEditViewModelValidator : AbstractValidator<LocalLibraryEditViewModel>
-{
-    public LocalLibraryEditViewModelValidator() => RuleFor(c => c.Name).NotEmpty();
-}
