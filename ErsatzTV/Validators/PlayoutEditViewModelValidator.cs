e0df454ac633a58079b118928dc8729efe2090be
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-07-11 21:08:37 +0000

    more layout updates for mobile (#2137)
    * update trakt, filler, filler editor ui
    
    * update schedules and playouts
    
    * update playout editor
    
    * update dependencies
    
    * update yaml playout editor
    
    * update path replacement editor

--- ErsatzTV/Validators/PlayoutEditViewModelValidator.cs
+++ ErsatzTV/Validators/PlayoutEditViewModelValidator.cs
@@ -1,5 +1,6 @@
 ﻿using ErsatzTV.ViewModels;
 using FluentValidation;
+using FluentValidation.Results;
 
 namespace ErsatzTV.Validators;
 
@@ -11,4 +12,18 @@ public class PlayoutEditViewModelValidator : AbstractValidator<PlayoutEditViewMo
         RuleFor(p => p.ProgramSchedule).NotNull().When(p => string.IsNullOrWhiteSpace(p.Kind));
         RuleFor(p => p.ExternalJsonFile).NotNull().When(p => p.Kind == PlayoutKind.ExternalJson);
     }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        ValidationResult result = await ValidateAsync(
+            ValidationContext<PlayoutEditViewModel>.CreateWithOptions(
+                (PlayoutEditViewModel)model,
+                x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+        {
+            return [];
+        }
+
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
 }
