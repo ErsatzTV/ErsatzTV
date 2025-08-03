8b12ee459ac4efde630234d4dc72fbade0d05201
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-08-03 20:26:16 +0000

    fix transitions on nvidia, vaapi, qsv (#2247)

--- ErsatzTV.FFmpeg/Filter/AudioFirstPtsFilter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ErsatzTV.FFmpeg.Filter;
-
-public class AudioFirstPtsFilter(int pts) : BaseFilter
-{
-    public override string Filter => $"aresample=async=1:first_pts={pts}";
-
-    public override FrameState NextState(FrameState currentState) => currentState;
-}
---
+++ ErsatzTV.FFmpeg/Filter/AudioResampleFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace ErsatzTV.FFmpeg.Filter;
+
+public class AudioResampleFilter : BaseFilter
+{
+    public override string Filter => "aresample=async=1";
+
+    public override FrameState NextState(FrameState currentState) => currentState;
+}
