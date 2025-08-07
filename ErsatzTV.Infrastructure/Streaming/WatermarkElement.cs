adec29913615caa7af5db6a3749cae71863ceecc
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-08-07 15:42:00 -0500

    add linear fade functions to watermark opacity expression

--- ErsatzTV.Infrastructure/Streaming/WatermarkElement.cs
+++ ErsatzTV.Infrastructure/Streaming/WatermarkElement.cs
@@ -4,6 +4,7 @@ using ErsatzTV.Core.FFmpeg;
 using ErsatzTV.Core.Interfaces.Streaming;
 using ErsatzTV.FFmpeg.State;
 using NCalc;
+using NCalc.Handlers;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Processing;
@@ -68,6 +69,8 @@ public class WatermarkElement : IGraphicsElement, IDisposable
             _expression = new Expression(opacity.ToString(CultureInfo.InvariantCulture));
         }
 
+        _expression.EvaluateFunction += EvaluateFunction;
+
         bool isRemoteUri = Uri.TryCreate(_imagePath, UriKind.Absolute, out var uriResult)
                            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
@@ -114,6 +117,40 @@ public class WatermarkElement : IGraphicsElement, IDisposable
             var frameDelay = _sourceImage.Frames[i].Metadata.GetFormatMetadata(GifFormat.Instance).FrameDelay / 100.0;
             _animatedDurationSeconds += frameDelay;
             _frameDelays.Add(frameDelay);
+        }
+    }
+
+    private static void EvaluateFunction(string name, FunctionArgs args)
+    {
+        switch (name)
+        {
+            case "LinearFadePoints":
+            {
+                if (args.Parameters.Length != 5)
+                    throw new ArgumentException("LinearFadePoints() requires 5 arguments.");
+
+                double time = Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.CurrentCulture);
+                double start = Convert.ToDouble(args.Parameters[1].Evaluate(), CultureInfo.CurrentCulture);
+                double peakStart = Convert.ToDouble(args.Parameters[2].Evaluate(), CultureInfo.CurrentCulture);
+                double peakEnd = Convert.ToDouble(args.Parameters[3].Evaluate(), CultureInfo.CurrentCulture);
+                double end = Convert.ToDouble(args.Parameters[4].Evaluate(), CultureInfo.CurrentCulture);
+
+                args.Result = LinearFadePoints(time, start, peakStart, peakEnd, end);
+                break;
+            }
+            case "LinearFadeDuration":
+            {
+                if (args.Parameters.Length != 4)
+                    throw new ArgumentException("LinearFadeDuration() requires 4 arguments.");
+
+                double time = Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.CurrentCulture);
+                double start = Convert.ToDouble(args.Parameters[1].Evaluate(), CultureInfo.CurrentCulture);
+                double fadeSeconds = Convert.ToDouble(args.Parameters[2].Evaluate(), CultureInfo.CurrentCulture);
+                double peakSeconds = Convert.ToDouble(args.Parameters[3].Evaluate(), CultureInfo.CurrentCulture);
+
+                args.Result = LinearFadeDuration(time, start, fadeSeconds, peakSeconds);
+                break;
+            }
         }
     }
 
@@ -196,6 +233,45 @@ public class WatermarkElement : IGraphicsElement, IDisposable
                 frameWidth - scaledWidth - horizontalMargin,
                 frameHeight - scaledHeight - verticalMargin),
         };
+    }
+
+    private static double LinearFadePoints(double time, double start, double peakStart, double peakEnd, double end)
+    {
+        if (time < start || time >= end)
+        {
+            return 0;
+        }
+
+        // fade in
+        if (time < peakStart)
+        {
+            return (time - start) / (peakStart - start);
+        }
+
+        // solid
+        if (time < peakEnd)
+        {
+            return 1.0;
+        }
+
+        // fade out
+        return (end - time) / (end - peakEnd);
+    }
+
+    private static double LinearFadeDuration(double time, double start, double fadeSeconds, double peakSeconds)
+    {
+        // edge case with no fade
+        if (fadeSeconds <= 0)
+        {
+            double noFadeEnd = start + peakSeconds;
+            return (time >= start && time < noFadeEnd) ? 1.0 : 0.0;
+        }
+
+        double peakStart = start + fadeSeconds;
+        double peakEnd = peakStart + peakSeconds;
+        double end = peakEnd + fadeSeconds;
+
+        return LinearFadePoints(time, start, peakStart, peakEnd, end);
     }
 
     public void Dispose()
