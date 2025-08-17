140d0803734c3e409c2b6a76f0d7dd66f4db2c08
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-08-17 08:12:35 -0500

    fix graphics engine overlay performance

--- ErsatzTV.FFmpeg/Filter/OverlayGraphicsEngineFilter.cs
+++ ErsatzTV.FFmpeg/Filter/OverlayGraphicsEngineFilter.cs
@@ -5,7 +5,7 @@ namespace ErsatzTV.FFmpeg.Filter;
 public class OverlayGraphicsEngineFilter(IPixelFormat outputPixelFormat) : BaseFilter
 {
     public override string Filter =>
-        $"overlay=format={(outputPixelFormat.BitDepth == 10 ? '1' : '0')}:alpha=premultiplied";
+        $"overlay=format={(outputPixelFormat.BitDepth == 10 ? '1' : '0')}";
 
     public override FrameState NextState(FrameState currentState) =>
         currentState with { FrameDataLocation = FrameDataLocation.Software };
