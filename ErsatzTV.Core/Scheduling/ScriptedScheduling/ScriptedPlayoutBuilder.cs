6771aa524029289972346c039831aa1ebbc52ead
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-08-23 13:45:08 -0500

    wip - not functional yet

--- ErsatzTV.Core/Scheduling/ScriptedScheduling/ScriptedPlayoutBuilder.cs
+++ ErsatzTV.Core/Scheduling/ScriptedScheduling/ScriptedPlayoutBuilder.cs
@@ -1,10 +1,19 @@
 using ErsatzTV.Core.Domain;
+using ErsatzTV.Core.Interfaces.Metadata;
 using ErsatzTV.Core.Interfaces.Scheduling;
+using ErsatzTV.Core.Scheduling.ScriptedScheduling.Modules;
+using IronPython.Hosting;
+using IronPython.Runtime;
 using Microsoft.Extensions.Logging;
 
 namespace ErsatzTV.Core.Scheduling.ScriptedScheduling;
 
-public class ScriptedPlayoutBuilder(ILogger<ScriptedPlayoutBuilder> logger) : IScriptedPlayoutBuilder
+public class ScriptedPlayoutBuilder(
+    //IConfigElementRepository configElementRepository,
+    //IMediaCollectionRepository mediaCollectionRepository,
+    ILocalFileSystem localFileSystem,
+    ILogger<ScriptedPlayoutBuilder> logger)
+    : IScriptedPlayoutBuilder
 {
     public async Task<PlayoutBuildResult> Build(
         DateTimeOffset start,
@@ -15,8 +24,53 @@ public class ScriptedPlayoutBuilder(ILogger<ScriptedPlayoutBuilder> logger) : IS
     {
         await Task.Delay(10, cancellationToken);
 
+        var result = PlayoutBuildResult.Empty;
+
+        if (!localFileSystem.FileExists(playout.ScheduleFile))
+        {
+            logger.LogError("Cannot build scripted playout; schedule file {File} does not exist", playout.ScheduleFile);
+            return result;
+        }
+
         logger.LogInformation("Building scripted playout...");
 
-        return PlayoutBuildResult.Empty;
+        //int daysToBuild = await GetDaysToBuild();
+        //DateTimeOffset finish = start.AddDays(daysToBuild);
+
+        //var enumeratorCache = new EnumeratorCache(mediaCollectionRepository, logger);
+
+        // apply all history???
+
+        try
+        {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+
+            var sys = engine.GetSysModule();
+            var modules = (PythonDictionary)sys.GetVariable("modules");
+
+            var types = engine.ImportModule("types");
+            dynamic moduleType = types.GetVariable("ModuleType");
+
+            dynamic ersatztv = engine.Operations.Invoke(moduleType, "ersatztv");
+            modules["ersatztv"] = ersatztv;
+
+            var contentModule = new ContentModule();
+            engine.Operations.SetMember(ersatztv, "content", contentModule);
+
+            engine.ExecuteFile(playout.ScheduleFile, scope);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error building scripted playout");
+            throw;
+        }
+
+        return result;
     }
+
+    // private async Task<int> GetDaysToBuild() =>
+    //     await configElementRepository
+    //         .GetValue<int>(ConfigElementKey.PlayoutDaysToBuild)
+    //         .IfNoneAsync(2);
 }
