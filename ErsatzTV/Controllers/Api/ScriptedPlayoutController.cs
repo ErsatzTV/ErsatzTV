54675d255e6bacd5a0a011c5d7147c9f7b736a17
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-09-03 14:32:50 -0500

    add all content fns

--- ErsatzTV/Controllers/Api/ScriptedPlayoutController.cs
+++ ErsatzTV/Controllers/Api/ScriptedPlayoutController.cs
@@ -56,6 +56,131 @@ public class ScriptedPlayoutController(IScriptedPlayoutBuilderService scriptedPl
         return Ok();
     }
 
+    [HttpPost("add_marathon", Name = "AddMarathon")]
+    public async Task<IActionResult> AddMarathon(
+        [FromRoute]
+        Guid buildId,
+        string key,
+        string groupBy,
+        string itemOrder = "shuffle",
+        Dictionary<string, List<string>> guids = null,
+        List<string> searches = null,
+        bool playAllItems = false,
+        bool shuffleGroups = false)
+    {
+        ISchedulingEngine engine = scriptedPlayoutBuilderService.GetEngine(buildId);
+        if (engine == null)
+        {
+            return NotFound($"Active build engine not found for build {buildId}.");
+        }
+
+        if (!Enum.TryParse(itemOrder, ignoreCase: true, out PlaybackOrder itemPlaybackOrder))
+        {
+            return BadRequest("Invalid item playback order.");
+        }
+
+        await engine.AddMarathon(
+            key,
+            guids ?? [],
+            searches ?? [],
+            groupBy,
+            shuffleGroups,
+            itemPlaybackOrder,
+            playAllItems);
+        return Ok();
+    }
+
+    [HttpPost("add_multi_collection", Name = "AddMultiCollection")]
+    public async Task<IActionResult> AddMultiCollection(
+        [FromRoute]
+        Guid buildId,
+        string key,
+        string multiCollection,
+        string order,
+        CancellationToken cancellationToken)
+    {
+        ISchedulingEngine engine = scriptedPlayoutBuilderService.GetEngine(buildId);
+        if (engine == null)
+        {
+            return NotFound($"Active build engine not found for build {buildId}.");
+        }
+
+        if (!Enum.TryParse(order, ignoreCase: true, out PlaybackOrder playbackOrder))
+        {
+            return BadRequest("Invalid playback order.");
+        }
+
+        await engine.AddMultiCollection(key, multiCollection, playbackOrder, cancellationToken);
+        return Ok();
+    }
+
+    [HttpPost("add_playlist", Name = "AddPlaylist")]
+    public async Task<IActionResult> AddPlaylist(
+        [FromRoute]
+        Guid buildId,
+        string key,
+        string playlist,
+        string playlistGroup,
+        CancellationToken cancellationToken)
+    {
+        ISchedulingEngine engine = scriptedPlayoutBuilderService.GetEngine(buildId);
+        if (engine == null)
+        {
+            return NotFound($"Active build engine not found for build {buildId}.");
+        }
+
+        await engine.AddPlaylist(key, playlist, playlistGroup, cancellationToken);
+        return Ok();
+    }
+
+    [HttpPost("add_smart_collection", Name = "AddSmartCollection")]
+    public async Task<IActionResult> AddSmartCollection(
+        [FromRoute]
+        Guid buildId,
+        string key,
+        string smartCollection,
+        string order,
+        CancellationToken cancellationToken)
+    {
+        ISchedulingEngine engine = scriptedPlayoutBuilderService.GetEngine(buildId);
+        if (engine == null)
+        {
+            return NotFound($"Active build engine not found for build {buildId}.");
+        }
+
+        if (!Enum.TryParse(order, ignoreCase: true, out PlaybackOrder playbackOrder))
+        {
+            return BadRequest("Invalid playback order.");
+        }
+
+        await engine.AddSmartCollection(key, smartCollection, playbackOrder, cancellationToken);
+        return Ok();
+    }
+
+    [HttpPost("add_show", Name = "AddShow")]
+    public async Task<IActionResult> AddShow(
+        [FromRoute]
+        Guid buildId,
+        string key,
+        Dictionary<string, string> guids,
+        string order,
+        CancellationToken cancellationToken)
+    {
+        ISchedulingEngine engine = scriptedPlayoutBuilderService.GetEngine(buildId);
+        if (engine == null)
+        {
+            return NotFound($"Active build engine not found for build {buildId}.");
+        }
+
+        if (!Enum.TryParse(order, ignoreCase: true, out PlaybackOrder playbackOrder))
+        {
+            return BadRequest("Invalid playback order.");
+        }
+
+        await engine.AddShow(key, guids, playbackOrder);
+        return Ok();
+    }
+
     [HttpPost("add_count", Name = "AddCount")]
     public IActionResult AddCount(
         [FromRoute]
