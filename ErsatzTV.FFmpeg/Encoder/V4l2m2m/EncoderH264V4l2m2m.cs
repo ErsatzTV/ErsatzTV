385d8959b624707c0d91e1822b26a56c90b30156
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-09-03 09:24:37 -0500

    more buffers

--- ErsatzTV.FFmpeg/Encoder/V4l2m2m/EncoderH264V4l2m2m.cs
+++ ErsatzTV.FFmpeg/Encoder/V4l2m2m/EncoderH264V4l2m2m.cs
@@ -7,6 +7,12 @@ public class EncoderH264V4l2m2m : EncoderBase
     public override string Name => "h264_v4l2m2m";
     public override StreamKind Kind => StreamKind.Video;
 
+    public override string[] OutputOptions =>
+    [
+        "-c:v", Name,
+        "-num_capture_buffers", "16"
+    ];
+
     public override FrameState NextState(FrameState currentState) => currentState with
     {
         VideoFormat = VideoFormat.H264,
