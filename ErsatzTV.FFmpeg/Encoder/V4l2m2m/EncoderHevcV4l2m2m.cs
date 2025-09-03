3e67da15c705d9688f0eb4c34fc82a10e3c60a3e
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-09-02 19:39:54 -0500

    add v4l2m2m pipeline

---
+++ ErsatzTV.FFmpeg/Encoder/V4l2m2m/EncoderHevcV4l2m2m.cs
@@ -0,0 +1,15 @@
+﻿using ErsatzTV.FFmpeg.Format;
+
+namespace ErsatzTV.FFmpeg.Encoder.V4l2m2m;
+
+public class EncoderHevcV4l2m2m : EncoderBase
+{
+    public override string Name => "hevc_V4l2m2m";
+    public override StreamKind Kind => StreamKind.Video;
+
+    public override FrameState NextState(FrameState currentState) => currentState with
+    {
+        VideoFormat = VideoFormat.Hevc,
+        FrameDataLocation = FrameDataLocation.Hardware
+    };
+}
