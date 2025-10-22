82e0fcaec854fe6bca41cd6b19808635e7da28bc
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-10-22 14:27:01 -0500

    maintain source fps when using qsv (#2558)

--- ErsatzTV.FFmpeg/Filter/ResetPtsFilter.cs
+++ ErsatzTV.FFmpeg/Filter/ResetPtsFilter.cs
@@ -1,8 +1,8 @@
 namespace ErsatzTV.FFmpeg.Filter;
 
-public class ResetPtsFilter : BaseFilter
+public class ResetPtsFilter(string fps) : BaseFilter
 {
-    public override string Filter => "setpts=PTS-STARTPTS";
+    public override string Filter => $"setpts=PTS-STARTPTS,fps={fps}";
 
     public override FrameState NextState(FrameState currentState) => currentState;
 }
