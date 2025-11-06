d87561d1409f34ebc445eb39f42fc45840c64711
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-11-06 06:11:57 -0600

    update subtitle titles (#2605)

---
+++ ErsatzTV.Infrastructure.MySql/Migrations/20251106120601_Update_SubtitleTitle.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_SubtitleTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"UPDATE Subtitle s
+INNER JOIN EpisodeMetadata em ON em.Id = s.EpisodeMetadataId
+INNER JOIN MediaVersion mv ON mv.EpisodeId = em.EpisodeId
+INNER JOIN MediaStream ms ON ms.MediaVersionId = mv.Id AND ms.MediaStreamKind = 3 AND ms.`Index` = s.StreamIndex
+SET s.Title = ms.Title
+WHERE s.Title != ms.Title;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ ErsatzTV.Infrastructure.Sqlite/Migrations/20251106120837_Update_SubtitleTitle.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_SubtitleTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"WITH Match AS (
+                    SELECT s.Id AS SubtitleId, ms.Title AS NewTitle
+                    FROM Subtitle AS s
+                    JOIN EpisodeMetadata AS em ON em.Id = s.EpisodeMetadataId
+                    JOIN MediaVersion AS mv ON mv.EpisodeId = em.EpisodeId
+                    JOIN MediaStream AS ms ON ms.MediaVersionId = mv.Id
+                    WHERE ms.MediaStreamKind = 3
+                      AND ms.`Index` = s.StreamIndex
+                      AND s.Title != ms.Title
+                )
+                UPDATE Subtitle
+                SET Title = (SELECT NewTitle FROM Match WHERE Match.SubtitleId = Subtitle.Id)
+                WHERE Id IN (SELECT SubtitleId FROM Match);
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
