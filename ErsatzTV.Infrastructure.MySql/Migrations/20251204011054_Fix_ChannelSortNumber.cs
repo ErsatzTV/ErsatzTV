e9344f2e64559c76f126b2debb6baf881e75198c
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-12-03 19:17:36 -0600

    add migration to fix any incorrect channel sort numbers

---
+++ ErsatzTV.Infrastructure.MySql/Migrations/20251204011054_Fix_ChannelSortNumber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErsatzTV.Infrastructure.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_ChannelSortNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"UPDATE `Channel`
+                  SET `SortNumber` = CAST(`Number` AS DECIMAL(10,2))
+                  WHERE `Number` IS NOT NULL AND TRIM(`Number`) != ''");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
