2c2743a74cf79f3b9a17725cfefab2276bc80e16
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-04-18 11:22:51 -0500

    rename and cleanup (#1)
    * rename artifacts; remove github changelog
    
    * update dependencies
    
    * remove unused markdown view
    
    * fix app name in macos scripts
    
    * fix unit tests

--- ErsatzTV.Core/Interfaces/GitHub/IGitHubApiClient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ErsatzTV.Core.Interfaces.GitHub;
-
-public interface IGitHubApiClient
-{
-    Task<Either<BaseError, string>> GetLatestReleaseNotes(CancellationToken cancellationToken);
-    Task<Either<BaseError, string>> GetReleaseNotes(string tag, CancellationToken cancellationToken);
-}
--- ErsatzTV.Infrastructure/GitHub/Models/GitHubTag.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ErsatzTV.Infrastructure.GitHub.Models;
-
-public class GitHubTag
-{
-    public string Body { get; set; }
-}
